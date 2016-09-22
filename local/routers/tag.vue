385cfebab6b97da0399290438ee6300d5392d5a3
梁灏 <admin@aresn.com>
2016-09-22 17:10:01 +0800

    add Timeline UI
    add Timeline UI

--- local/routers/tag.vue
+++ local/routers/tag.vue
@@ -6,34 +6,73 @@
         <Button @click="error">error</Button>
         <Button @click="update">update</Button>
         <br><br>
-        <Progress :percent="25"></Progress>
-        <Progress :percent="45" status="success"></Progress>
-        <Progress :percent="45" status="active"></Progress>
-        <Progress :percent="65" status="wrong"></Progress>
-        <Progress :percent="100"></Progress>
-        <Progress :percent="25" hide-info></Progress>
-        <Progress :percent="percent"></Progress>
-        <Button-group size="large">
-            <Button icon="ios-plus-empty" @click="add"></Button>
-            <Button icon="ios-minus-empty" @click="minus"></Button>
-        </Button-group>
-        <Progress :percent="25" :stroke-width="5"></Progress>
-        <Progress :percent="100">
-            <Icon type="checkmark-circled"></Icon>
-            <span>成功</span>
-        </Progress>
+        <Timeline>
+            <Timeline-item>
+                <p class="time">1976年</p>
+                <p class="content">Apple I 问世</p>
+            </Timeline-item>
+            <Timeline-item>
+                <p class="time">1984年</p>
+                <p class="content">发布 Macintosh</p>
+            </Timeline-item>
+            <Timeline-item>
+                <p class="time">2007年</p>
+                <p class="content">发布 iPhone</p>
+            </Timeline-item>
+            <Timeline-item>
+                <p class="time">2010年</p>
+                <p class="content">发布 iPad</p>
+            </Timeline-item>
+            <Timeline-item>
+                <p class="time">2011年10月5日</p>
+                <p class="content">史蒂夫·乔布斯去世</p>
+            </Timeline-item>
+        </Timeline>
+        <br><br><br>
+        <Timeline>
+            <Timeline-item color="green">发布1.0版本</Timeline-item>
+            <Timeline-item color="green">发布2.0版本</Timeline-item>
+            <Timeline-item color="red">严重故障</Timeline-item>
+            <Timeline-item color="blue">发布3.0版本</Timeline-item>
+        </Timeline>
+        <br><br><br>
+        <Timeline pending>
+            <Timeline-item>发布1.0版本</Timeline-item>
+            <Timeline-item>发布2.0版本</Timeline-item>
+            <Timeline-item>发布3.0版本</Timeline-item>
+            <Timeline-item><a href="#">查看更多</a></Timeline-item>
+        </Timeline>
+        <br><br><br>
+        <Timeline>
+            <Timeline-item color="green">
+                <Icon type="trophy" slot="dot"></Icon>
+                <span>发布里程碑版本</span>
+            </Timeline-item>
+            <Timeline-item>发布1.0版本</Timeline-item>
+            <Timeline-item>发布2.0版本</Timeline-item>
+            <Timeline-item>发布3.0版本</Timeline-item>
+        </Timeline>
+        <br><br><br>
+        <Timeline pending>
+            <Timeline-item>发布1.0版本</Timeline-item>
+            <Timeline-item>发布2.0版本</Timeline-item>
+        </Timeline>
+        <br><br><br>
     </div>
 </template>
 <script>
-    import { Tag, LoadingBar, Button, Progress, Icon } from 'iview';
+    import { Tag, LoadingBar, Button, Progress, Icon, Timeline } from 'iview';
     const ButtonGroup = Button.Group;
+    const TimelineItem = Timeline.Item;
     export default {
         components: {
             Tag,
             Button,
             Progress,
             ButtonGroup,
-            Icon
+            Icon,
+            Timeline,
+            TimelineItem
         },
         props: {
         
