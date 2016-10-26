bf962a69178c7f46526f4803aea68625f153c88d
梁灏 <admin@aresn.com>
2016-10-26 12:04:40 +0800

    remove Row css - position:relative
    remove Row css - position:relative

--- local/routers/notice.vue
+++ local/routers/notice.vue
@@ -4,21 +4,35 @@
         padding: 20px;
     }
     body{
+        height: 1000px;
         padding: 10px;
     }
 </style>
 <template>
     <div class="tooltip_out">
-        <Tooltip content="Top Left 提示文字" placement="top-start">
-            <Button>上面的左面</Button>
-        </Tooltip>
+        123
     </div>
+    <div class="tooltip_out">
+
+    </div>
+    <Row>
+        <i-col span="12">
+            <Tooltip content="Top Left 提示文字" placement="top-start">
+                <Button>上面的左面</Button>
+            </Tooltip>
+        </i-col>
+        <i-col span="12">
+            <Tooltip content="Top Left 提示文字" placement="top-start">
+                <Button>上面的左面</Button>
+            </Tooltip>
+        </i-col>
+    </Row>
 </template>
 <script>
-    import { Tooltip, Button } from 'iview';
+    import { Tooltip, Button, Row, iCol } from 'iview';
 
     export default {
-        components: { Tooltip, Button },
+        components: { Tooltip, Button, Row, iCol },
         props: {
         
         },
