9b7840163d5e7edd9ce4374f5d67cbe67e4cf8fa
梁灏 <admin@aresn.com>
2016-10-27 11:45:21 +0800

    update Poptip styles
    update Poptip styles

--- local/routers/notice.vue
+++ local/routers/notice.vue
@@ -6,9 +6,71 @@
         height: 1000px;
         padding: 10px;
     }
+    .api table{
+        font-family: Consolas,Menlo,Courier,monospace;
+        font-size: 13px;
+        border-collapse: collapse;
+        border-spacing: 0;
+        empty-cells: show;
+        border: 1px solid #e9e9e9;
+        width: 100%;
+        margin-bottom: 24px;
+    }
+    .api table th{
+        background: #f7f7f7;
+        white-space: nowrap;
+        color: #5c6b77;
+        font-weight: 600;
+    }
+    .api table td, .api table th{
+        border: 1px solid #e9e9e9;
+        padding: 8px 16px;
+        text-align: left;
+    }
 </style>
 <template>
     <div class="tooltip_out">
+        <Poptip placement="right" width="300">
+            <Button type="ghost">click 激活</Button>
+            <div class="api" slot="content">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>属性</th>
+                        <th>说明</th>
+                        <th>类型</th>
+                        <th>默认值</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>content</td>
+                        <td>显示的内容</td>
+                        <td>String | Number</td>
+                        <td>空</td>
+                    </tr>
+                    <tr>
+                        <td>placement</td>
+                        <td>提示框出现的位置，可选值为<code>top</code><code>top-start</code><code>top-end</code><code>bottom</code><code>bottom-start</code><code>bottom-end</code><code>left</code><code>left-start</code><code>left-end</code><code>right</code><code>right-start</code><code>right-end</code></td>
+                        <td>String</td>
+                        <td>bottom</td>
+                    </tr>
+                    <tr>
+                        <td>disabled</td>
+                        <td>是否禁用提示框</td>
+                        <td>Boolean</td>
+                        <td>false</td>
+                    </tr>
+                    <tr>
+                        <td>delay</td>
+                        <td>延迟显示，单位毫秒</td>
+                        <td>Number</td>
+                        <td>0</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </Poptip>
         <!--<Poptip title="标题" content="内容">-->
             <!--<Button>click 触发</Button>-->
         <!--</Poptip>-->
