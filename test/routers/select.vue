9a9e103f4ce382155a231820a3032eb6a72ed2d3
梁灏 <admin@aresn.com>
2016-11-07 15:55:05 +0800

    optimize Select style
    optimize Select style

--- test/routers/select.vue
+++ test/routers/select.vue
@@ -1,26 +1,44 @@
 <template>
-    <!--<i-select :model.sync="model1" style="width:200px">-->
-        <!--<i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>-->
-    <!--</i-select>-->
-    <!--{{ model1 | json }}-->
+    <i-select :model.sync="model1" style="width:200px" clearable>
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    {{ model1 | json }}
     <i-button @click="change">修改数据</i-button>
-    <!--<i-select :model.sync="model10" multiple style="width:240px" @on-change="datachange">-->
-        <!--<i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>-->
-    <!--</i-select>-->
-    <!--{{ model10 | json }}-->
-    <!--<i-select :model.sync="model11" filterable style="width:200px" @on-change="datachange">-->
-        <!--<i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>-->
-    <!--</i-select>-->
-    <!--{{ model11 | json }}-->
+    <i-select :model.sync="model10" multiple style="width:240px" @on-change="datachange">
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    {{ model10 | json }}
+    <i-select :model.sync="model11" filterable style="width:200px" @on-change="datachange">
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    {{ model11 | json }}
     <i-select :model.sync="model12" filterable multiple style="width:240px" @on-change="datachange">
         <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
     </i-select>
     {{ model12 | json }}
+    <br><br>
+    <i-select :model.sync="model2" size="small" style="width:100px">
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    <i-select :model.sync="model3" style="width:100px">
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    <i-select :model.sync="model4" size="large" style="width:100px">
+        <i-option v-for="item in cityList" :value="item.value">{{ item.label }}</i-option>
+    </i-select>
+    <i-select :model.sync="model7" style="width:200px">
+        <i-option-group label="热门城市">
+            <i-option v-for="item in cityList | limitBy 3" :value="item.value">{{ item.label }}</i-option>
+        </i-option-group>
+        <i-option-group label="其它城市">
+            <i-option v-for="item in cityList | limitBy 3 3" :value="item.value">{{ item.label }}</i-option>
+        </i-option-group>
+    </i-select>
 </template>
 <script>
-    import { iSelect, iOption, iButton } from 'iview';
+    import { iSelect, iOption, iButton, iOptionGroup } from 'iview';
     export default {
-        components: { iSelect, iOption, iButton },
+        components: { iSelect, iOption, iButton, iOptionGroup },
         data () {
             return {
                 cityList: [
@@ -52,7 +70,11 @@
                 model1: '',
                 model10: [],
                 model11: '',
-                model12: []
+                model12: [],
+                model2: '',
+                model3: '',
+                model4: '',
+                model7: ''
             }
         },
         methods: {
