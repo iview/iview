9fc59aa69ce8f072da5a0fdb5a8ab89257bcebb5
梁灏 <admin@aresn.com>
2016-12-07 10:20:11 +0800

    update Tabs
    update Tabs

--- test/routers/tabs.vue
+++ test/routers/tabs.vue
@@ -1,16 +1,18 @@
 <template>
     <i-button @click="toggleClose">closable</i-button>
 
-    <Tabs type="line" :closable="closable" :animated="false">
-        <Tab-pane label="Tab 1">Tab1 content</Tab-pane>
-        <Tab-pane label="Tab 2" icon="ionic">Tab2 content</Tab-pane>
-        <Tab-pane label="Tab 3" disabled>Tab3 content</Tab-pane>
-        <Tab-pane label="Tab 4">Tab4 content</Tab-pane>
-        <Tab-pane label="Tab 5">Tab5 content</Tab-pane>
-        <Tab-pane label="Tab 6" icon="ionic">Tab6 content</Tab-pane>
-        <Tab-pane label="Tab 7" disabled>Tab7 content</Tab-pane>
-        <Tab-pane label="Tab 888888888">Tab8 content</Tab-pane>
-    </Tabs>
+    <div style="margin:20px;width:400px">
+        <Tabs type="line" :closable="closable" 3399ff>
+            <Tab-pane label="Tab 1">Tab1 content</Tab-pane>
+            <Tab-pane label="Tab 2" icon="ionic">Tab2 content</Tab-pane>
+            <Tab-pane label="Tab 3" disabled>Tab3 content</Tab-pane>
+            <Tab-pane label="Tab 4">Tab4 content</Tab-pane>
+            <Tab-pane label="Tab 5">Tab5 content</Tab-pane>
+            <Tab-pane label="Tab 6" icon="ionic">Tab6 content</Tab-pane>
+            <Tab-pane label="Tab 7" disabled>Tab7 content</Tab-pane>
+            <Tab-pane label="Tab 888888888">Tab8 content</Tab-pane>
+        </Tabs>
+    </div>
 </template>
 <script>
     export default {
