9358a9ae2715c1a0680679c4f5cea9b82f4bd450
梁灏 <admin@aresn.com>
2016-12-23 09:12:34 +0800

    release 0.9.11
    release 0.9.11

--- test/routers/date.vue
+++ test/routers/date.vue
@@ -1,5 +1,5 @@
 <template>
-    <date-picker confirm :open="open" :value="value3" @on-change="handleChange" type="date" @on-open-change="c" @on-clear="clear" @on-ok="ok">
+    <date-picker readonly confirm format="MM/dd/yy" :open="open" :value="value3" @on-change="handleChange" type="date" @on-open-change="c" @on-clear="clear" @on-ok="ok">
         <a href="javascript:void(0)" @click="handleClick">
             <Icon type="ios-calendar-outline"></Icon>
             <template v-if="value3 === ''">选择日期</template>
