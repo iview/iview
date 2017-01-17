2171f454fd706610ab7b671e6e3731a0760a2472
Rijn <bxbian951122@gmail.com>
2017-01-17 16:44:50 -0600

    added many test

--- test/routers/carousel.vue
+++ test/routers/carousel.vue
@@ -74,9 +74,29 @@
             </Alert>
         </Carousel-item>
         <Carousel-item>
-            <div style="height: 100%; min-height: 20px; background: #f50; position: relative;">
+            <div style="height: 150px; background: #f50; position: relative">
                 <p style="position: absolute; width: 100%; color: #fff; top: 50%; height: 20px; line-height: 20px; margin-top: -10px; text-align: center">test font 定高测试</p>
             </div>
+        </Carousel-item>
+        <Carousel-item>
+            <div style="height: 100%; min-height: 20px; background: #f50; position: relative;">
+                <p style="position: absolute; width: 100%; color: #fff; top: 50%; height: 20px; line-height: 20px; margin-top: -10px; text-align: center">test font 居中测试</p>
+            </div>
+        </Carousel-item>
+        <Carousel-item>
+            <Card style="width:350px">
+                <p slot="title">
+                    <Icon type="ios-film-outline"></Icon>
+                    经典电影
+                </p>
+                <a href="#" slot="extra">
+                    <Icon type="ios-loop-strong"></Icon>
+                    换一换
+                </a>
+                <ul>
+
+                </ul>
+            </Card>
         </Carousel-item>
         <Carousel-item style="text-align: center">
             <Icon type="checkmark" style="font-size: 5em"></Icon>
@@ -86,9 +106,77 @@
             <Icon type="checkmark" style="font-size: 5em"></Icon>{{item}}
         </Carousel-item>
     </Carousel>
-    <div>
+    <div style="max-height: 200px; overflow: scroll">
         <p v-for="item in log" track-by="$index">{{item}}</p>
     </div>
+
+    <Card style="width:350px">
+        <p slot="title">
+            <Icon type="ios-film-outline"></Icon>
+            经典电影
+        </p>
+        <Carousel>
+            <Carousel-item v-if="!remove">
+                <Alert type="warning" show-icon>
+                    警告提示文案
+                    <template slot="desc">
+                        警告的提示描述文案警告的提示描述文案警告的提示描述文案
+                    </template>
+                </Alert>
+            </Carousel-item>
+            <Carousel-item>
+                <div style="height: 100%; min-height: 20px; background: #f50; position: relative;">
+                    <p style="position: absolute; width: 100%; color: #fff; top: 50%; height: 20px; line-height: 20px; margin-top: -10px; text-align: center">test font 定高测试</p>
+                </div>
+            </Carousel-item>
+            <Carousel-item style="text-align: center">
+                <Icon type="checkmark" style="font-size: 5em"></Icon>
+            </Carousel-item>
+        </Carousel>
+    </Card>
+    <Tabs>
+        <Tab-pane label="标签一">
+            <Carousel>
+                <Carousel-item v-if="!remove">
+                    <Alert type="warning" show-icon>
+                        警告提示文案
+                        <template slot="desc">
+                            警告的提示描述文案警告的提示描述文案警告的提示描述文案
+                        </template>
+                    </Alert>
+                </Carousel-item>
+                <Carousel-item>
+                    <div style="height: 100%; min-height: 20px; background: #f50; position: relative;">
+                        <p style="position: absolute; width: 100%; color: #fff; top: 50%; height: 20px; line-height: 20px; margin-top: -10px; text-align: center">test font 定高测试</p>
+                    </div>
+                </Carousel-item>
+                <Carousel-item style="text-align: center">
+                    <Icon type="checkmark" style="font-size: 5em"></Icon>
+                </Carousel-item>
+            </Carousel>
+        </Tab-pane>
+        <Tab-pane label="标签二" disabled>标签二的内容</Tab-pane>
+        <Tab-pane label="标签三">
+            <Carousel>
+                <Carousel-item v-if="!remove">
+                    <Alert type="warning" show-icon>
+                        警告提示文案
+                        <template slot="desc">
+                            警告的提示描述文案警告的提示描述文案警告的提示描述文案
+                        </template>
+                    </Alert>
+                </Carousel-item>
+                <Carousel-item>
+                    <div style="height: 100%; min-height: 20px; background: #f50; position: relative;">
+                        <p style="position: absolute; width: 100%; color: #fff; top: 50%; height: 20px; line-height: 20px; margin-top: -10px; text-align: center">test font 定高测试</p>
+                    </div>
+                </Carousel-item>
+                <Carousel-item style="text-align: center">
+                    <Icon type="checkmark" style="font-size: 5em"></Icon>
+                </Carousel-item>
+            </Carousel>
+        </Tab-pane>
+    </Tabs>
 </template>
 <script>
     export default {
