b0bbb632eb94977b0978f79df3aa4fd1d5f60cb1
leonine <326026108@qq.com>
2017-02-20 10:01:37 +0800

    删除无用的

--- test/routers/table.vue
+++ test/routers/table.vue
@@ -74,38 +74,6 @@
                         address: '深圳市南山区深南大道',
                         _checked: true
                     }
-                ],
-                columns8:[
-                     {
-                        type: 'selection',
-                        width: 60,
-                        align: 'center'
-                    },
-                    {
-                        title: '名称',
-                        key: 'name'
-                    },
-                    {
-                        title: '年龄',
-                        key: 'age'
-                    },
-                    {
-                        title: '地址',
-                        key: 'address'
-                    }
-                ],
-                data8:[
-                     {
-                        name: '王小明',
-                        age: 18,
-                        address: '北京市朝阳区芍药居',
-                        checked:true  // 传checked 可以设置该行为默认选中状态
-                    },
-                    {
-                        name: '张小刚',
-                        age: 25,
-                        address: '北京市海淀区西二旗'
-                    }
                 ]
             }
         },
