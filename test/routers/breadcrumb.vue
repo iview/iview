469c0643e863815346e774d68e66e2fc97dd8a13
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2017-03-02 22:52:12 +0800

    Support Breadcrumb

---
+++ test/routers/breadcrumb.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <Breadcrumb>
+        <Breadcrumb-item href="/">Home</Breadcrumb-item>
+        <Breadcrumb-item href="/components/breadcrumb">Components</Breadcrumb-item>
+        <Breadcrumb-item>Breadcrumb</Breadcrumb-item>
+    </Breadcrumb>
+    <Breadcrumb>
+        <Breadcrumb-item href="/">
+            <Icon type="ios-home-outline"></Icon> Home
+        </Breadcrumb-item>
+        <Breadcrumb-item href="/components/breadcrumb">
+            <Icon type="social-buffer-outline"></Icon> Components
+        </Breadcrumb-item>
+        <Breadcrumb-item>
+            <Icon type="pound"></Icon> Breadcrumb
+        </Breadcrumb-item>
+    </Breadcrumb>
+   <Breadcrumb separator=">">
+        <Breadcrumb-item href="/">Home</Breadcrumb-item>
+        <Breadcrumb-item href="/components/breadcrumb">Components</Breadcrumb-item>
+        <Breadcrumb-item>Breadcrumb</Breadcrumb-item>
+    </Breadcrumb>
+    <Breadcrumb separator="<b class='demo-breadcrumb-separator'>=></b>">
+        <Breadcrumb-item href="/">Home4</Breadcrumb-item>
+        <Breadcrumb-item href="/components/breadcrumb">Components</Breadcrumb-item>
+        <Breadcrumb-item>Breadcrumb</Breadcrumb-item>
+    </Breadcrumb>
+    <Breadcrumb>
+        <Breadcrumb-item href="/">Home4</Breadcrumb-item>
+        <Breadcrumb-item href="/components/breadcrumb">Components</Breadcrumb-item>
+        <Breadcrumb-item>Breadcrumb</Breadcrumb-item>
+    </Breadcrumb>
+</div>
+</template>
+<style>
+    .demo-breadcrumb-separator{
+        color: #ff5500;
+        padding: 0 5px;
+    }
+</style>
+<script>
+    export default {
+
+    }
+</script>
