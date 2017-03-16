45f4b6d035ffdd6ae3335a5ecdff98641b28b590
Allen Walker <hjl.lapuda@gmail.com>
2017-03-16 16:11:27 +0800

    fix #402 support the Runtime-only build of Vue

--- src/components/base/notification/index.js
+++ src/components/base/notification/index.js
@@ -1,23 +1,17 @@
 import Notification from './notification.vue';
 import Vue from 'vue';
-import { camelcaseToHyphen } from '../../../utils/assist';
 
 Notification.newInstance = properties => {
     const _props = properties || {};
-
-    let props = '';
-    Object.keys(_props).forEach(prop => {
-        props += ' :' + camelcaseToHyphen(prop) + '=' + prop;
-    });
-
     const div = document.createElement('div');
-    div.innerHTML = `<notification${props}></notification>`;
     document.body.appendChild(div);
 
     const notification = new Vue({
         el: div,
-        data: _props,
-        components: { Notification }
+        components: { Notification },
+        render (h) {
+            return h('Notification', { props: Object.assign({}, _props) })
+        }
     }).$children[0];
 
     return {
