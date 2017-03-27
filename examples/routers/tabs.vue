c4eb5dcffd6ba38e939559ab35e10916900fa39c
H <21418592@qq.com>
2017-03-27 13:28:37 +0800

    tabs组件导航区添加右侧slot功能及修改例子文件

--- examples/routers/tabs.vue
+++ examples/routers/tabs.vue
@@ -1,14 +1,31 @@
 <template>
-    <Tabs type="card" closable @on-tab-remove="handleTagRemove">
-        <Tab-pane label="标签一" v-if="tab0">标签一的内容</Tab-pane>
-        <Tab-pane label="标签二" v-if="tab1">标签二的内容</Tab-pane>
-        <Tab-pane label="标签三" v-if="tab2">标签三的内容</Tab-pane>
-    </Tabs>
+    <Row>
+        <Col :span="12">
+            <Tabs type="card" closable @on-tab-remove="handleTagRemove">
+                <Tab-pane label="标签一" v-if="tab0">标签一的内容</Tab-pane>
+                <Tab-pane label="标签二" v-if="tab1">标签二的内容</Tab-pane>
+                <Tab-pane label="标签三" v-if="tab2">标签三的内容</Tab-pane>
+            </Tabs>
+        </Col>
+        <Col :span="12">
+            <Tabs type="card" closable @on-tab-remove="handleTagRemove">
+                <Tab-pane :label="tab.label" v-for="tab of tabs">{{tab.content}}</Tab-pane>
+                <Button size="small" slot="right" @click="addTab">添加</Button>
+            </Tabs>
+        </Col>
+    </Row>
 </template>
-<script>
+
+
+<script lang="babel">
     export default {
         data () {
             return {
+                tabs:[
+                        {label:'标签0',content:'标签内容0'},
+                        {label:'标签1',content:'标签内容1'},
+                        {label:'标签2',content:'标签内容2'},
+                ],
                 tab0: true,
                 tab1: true,
                 tab2: true
@@ -17,6 +34,9 @@
         methods: {
             handleTagRemove (name) {
                 this['tab' + name] = false;
+            },
+            addTab(){
+                this.tabs.push({label:'标签'+this.tabs.length,content:'标签内容'+this.tabs.length});
             }
         }
     }
