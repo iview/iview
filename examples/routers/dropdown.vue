70d5a4ab80ff73b234f15e543ed469e042272878
梁灏 <admin@aresn.com>
2017-03-27 10:48:18 +0800

    fixed #425

--- examples/routers/dropdown.vue
+++ examples/routers/dropdown.vue
@@ -1,35 +1,26 @@
 <template>
     <div>
-        <Dropdown>
+        <Dropdown @on-click="handleClick">
             <a href="javascript:void(0)">
                 下拉菜单
                 <Icon type="arrow-down-b"></Icon>
             </a>
             <Dropdown-menu slot="list">
-                <Dropdown-item>驴打滚</Dropdown-item>
-                <Dropdown-item>炸酱面</Dropdown-item>
-                <Dropdown-item disabled>豆汁儿</Dropdown-item>
-                <Dropdown-item>冰糖葫芦</Dropdown-item>
-                <Dropdown-item divided>北京烤鸭</Dropdown-item>
-            </Dropdown-menu>
-        </Dropdown>
-        <Dropdown style="margin-left: 20px">
-            <Button type="primary">
-                下拉菜单
-                <Icon type="arrow-down-b"></Icon>
-            </Button>
-            <Dropdown-menu slot="list">
-                <Dropdown-item>驴打滚</Dropdown-item>
-                <Dropdown-item>炸酱面</Dropdown-item>
-                <Dropdown-item disabled>豆汁儿</Dropdown-item>
-                <Dropdown-item>冰糖葫芦</Dropdown-item>
-                <Dropdown-item divided>北京烤鸭</Dropdown-item>
+                <Dropdown-item name="1">驴打滚</Dropdown-item>
+                <Dropdown-item name="2">炸酱面</Dropdown-item>
+                <Dropdown-item name="3" disabled>豆汁儿</Dropdown-item>
+                <Dropdown-item name="4">冰糖葫芦</Dropdown-item>
+                <Dropdown-item name="5" divided>北京烤鸭</Dropdown-item>
             </Dropdown-menu>
         </Dropdown>
     </div>
 </template>
 <script>
     export default {
-
+        methods: {
+            handleClick (val) {
+                console.log(val)
+            }
+        }
     }
 </script>
--- examples/routers/radio.vue
+++ examples/routers/radio.vue
@@ -1,26 +1,8 @@
 <template>
     <div>
-        <Radio-group v-model="phone">
-            <Row>
-                <i-col span="8">
-                    <Radio label="apple">
-                        <Icon type="social-apple"></Icon>
-                        <span>Apple</span>
-                    </Radio>
-                </i-col>
-                <i-col span="8">
-                    <Radio label="android">
-                        <Icon type="social-android"></Icon>
-                        <span>Android</span>
-                    </Radio>
-                </i-col>
-                <i-col span="8">
-                    <Radio label="windows">
-                        <Icon type="social-windows"></Icon>
-                        <span>Windows</span>
-                    </Radio>
-                </i-col>
-            </Row>
+        <Radio-group v-model="sex">
+            <Radio :label="0">男</Radio>
+            <Radio :label="1">女</Radio>
         </Radio-group>
     </div>
 </template>
@@ -28,8 +10,7 @@
     export default {
         data () {
             return {
-                phone: 'apple',
-                animal: '爪哇犀牛'
+                sex: 0
             }
         }
     }
