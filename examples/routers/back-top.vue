4a41025b56dd3b26f878b20a824542463980ed9a
se7en <373084212@qq.com>
2017-05-10 18:04:33 +0800

    扩展back-top组件，支持在容器内滚动也能出现back按钮 添加listen，传递需要监听的容器id

--- examples/routers/back-top.vue
+++ examples/routers/back-top.vue
@@ -2,11 +2,25 @@
     body{
         height: 2000px;
     }
+    .scroll-box{
+        height: 400px;
+        overflow: auto;
+    }
+    .scroll-list{
+        height: 2000px;
+        background: linear-gradient(to bottom,#3399ff,#f5f5f5);
+    }
 </style>
 <template>
-    <BackTop>
-       
-    </BackTop>
+<div>
+    <BackTop></BackTop>
+    <div class="scroll-box" id="scroll-box">
+        <div class="scroll-list">
+            <h1>scroll content</h1>
+        </div>
+        <BackTop listen="scroll-box"></BackTop>
+    </div>
+</div>
 </template>
 <script>
     export default {
