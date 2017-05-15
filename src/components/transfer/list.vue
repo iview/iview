195d4af514b1f5f17f053b581fc3a2f1d9497a9a
Rui Ma <xjmarui@gmail.com>
2017-05-15 15:59:23 +0800

    click transfer's title to toggle select status

--- src/components/transfer/list.vue
+++ src/components/transfer/list.vue
@@ -2,7 +2,7 @@
     <div :class="classes" :style="style">
         <div :class="prefixCls + '-header'">
             <Checkbox :value="checkedAll" :disabled="checkedAllDisabled" @on-change="toggleSelectAll"></Checkbox>
-            <span>{{ title }}</span>
+            <span @click="toggleSelectAll(!checkedAll)">{{ title }}</span>
             <span :class="prefixCls + '-header-count'">{{ count }}</span>
         </div>
         <div :class="bodyClasses">
