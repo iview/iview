dce5a3ea6b3e600a2973d0e7b4c17de14615bf68
Aresn <admin@aresn.com>
2017-06-01 19:00:37 +0800

    update Modal

--- examples/routers/modal.vue
+++ examples/routers/modal.vue
@@ -3,11 +3,32 @@
         <Button @click="confirm">标准</Button>
         <Button @click="custom">自定义按钮文字</Button>
         <Button @click="async">异步关闭</Button>
+        <Button type="primary" @click="modal1 = true">显示对话框</Button>
+        <Modal
+                v-model="modal1"
+                title="普通的Modal对话框标题"
+                @on-ok="ok"
+                @on-cancel="cancel">
+            <p>对话框内容</p>
+            <p>对话框内容</p>
+            <p>对话框内容</p>
+        </Modal>
     </div>
 </template>
 <script>
     export default {
+        data () {
+            return {
+                modal1: false
+            }
+        },
         methods: {
+            ok () {
+                this.$Message.info('点击了确定');
+            },
+            cancel () {
+                this.$Message.info('点击了取消');
+            },
             confirm () {
                 this.$Modal.confirm({
                     title: '确认对话框标题',
