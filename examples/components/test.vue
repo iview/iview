4098c176e1bcbd87541b1a620e712e25d0a4c303
Aresn <admin@aresn.com>
2017-06-09 16:44:24 +0800

    update Table

--- examples/components/test.vue
+++ examples/components/test.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>123</div>
+    <div>{{ row.name }}</div>
 </template>
 <script>
     export default {
-
+        props: {
+            row: Object
+        },
+        mounted () {
+            console.log(1);
+        },
+        beforeDestroy () {
+            console.log(2);
+        }
     }
 </script>
--- src/components/table/expand.js
+++ src/components/table/expand.js
@@ -6,8 +6,17 @@ export default {
         row: Object,
         render: Function,
         index: Number,
+        column: {
+            type: Object,
+            default: null
+        }
     },
     render: (h, ctx) => {
-        return ctx.props.render(h, {row: ctx.props.row, index: ctx.props.index});
+        const params = {
+            row: ctx.props.row,
+            index: ctx.props.index
+        };
+        if (ctx.props.column) params.column = ctx.props.column;
+        return ctx.props.render(h, params);
     }
 };
