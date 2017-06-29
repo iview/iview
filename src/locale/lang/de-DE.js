70d9ff170295dd79e5bbe7c86f0b7cea541f1e89
Danny Spangenberg <danny.spangenberg@twiceware.de>
2017-06-29 20:06:55 +0200

    German language file

---
+++ src/locale/lang/de-DE.js
@@ -0,0 +1,96 @@
+export default {
+    i: {
+        select: {
+            placeholder: 'Auswählen',
+            noMatch: 'Keine Übereinstimmungen',
+            loading: 'Lädt'
+        },
+        table: {
+            noDataText: 'Keine Daten',
+            noFilteredDataText: 'Keine gefilterten Daten',
+            confirmFilter: 'Bestätigen',
+            resetFilter: 'Zurücksetzen',
+            clearFilter: 'Alle'
+        },
+        datepicker: {
+            selectDate: 'Datum auswählen',
+            selectTime: 'Zeit auswählen',
+            startTime: 'Beginn',
+            endTime: 'Ende',
+            clear: 'Leeren',
+            ok: 'OK',
+            month: '',
+            month1: 'Januar',
+            month2: 'Februar',
+            month3: 'März',
+            month4: 'April',
+            month5: 'Mai',
+            month6: 'Juni',
+            month7: 'Juli',
+            month8: 'August',
+            month9: 'September',
+            month10: 'Oktober',
+            month11: 'November',
+            month12: 'Dezember',
+            year: '',
+            weeks: {
+                sun: 'So',
+                mon: 'Mo',
+                tue: 'Di',
+                wed: 'Mi',
+                thu: 'Do',
+                fri: 'Fr',
+                sat: 'Sa'
+            },
+            months: {
+                m1: 'Jan',
+                m2: 'Feb',
+                m3: 'Mär',
+                m4: 'Apr',
+                m5: 'Mai',
+                m6: 'Jun',
+                m7: 'Jul',
+                m8: 'Aug',
+                m9: 'Sep',
+                m10: 'Okt',
+                m11: 'Nov',
+                m12: 'Dez'
+            }
+        },
+        transfer: {
+            titles: {
+                source: 'Quelle',
+                target: 'Ziel'
+            },
+            filterPlaceholder: 'Suchen',
+            notFoundText: 'Nicht gefunden'
+        },
+        modal: {
+            okText: 'OK',
+            cancelText: 'Abbrechen'
+        },
+        poptip: {
+            okText: 'OK',
+            cancelText: 'Abbrechen'
+        },
+        page: {
+            prev: 'Vorherige Seite',
+            next: 'Nächste Seite',
+            total: 'Total',
+            item: 'Eintrag',
+            items: 'Einträge',
+            prev5: 'vorherigen fünf Seiten',
+            next5: 'nächste fünf Seiten',
+            page: '/seite',
+            goto: 'Gehe zu',
+            p: ''
+        },
+        rate: {
+            star: 'Stern',
+            stars: 'Sterne'
+        },
+        tree: {
+            emptyText: 'Keine Daten'
+        }
+    }
+};
