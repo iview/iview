e61c092eac1261be52856e9a46aadf8d17015571
Aresn <admin@aresn.com>
2017-06-30 16:51:28 +0800

    update Page style

--- examples/routers/page.vue
+++ examples/routers/page.vue
@@ -1,10 +1,14 @@
 <template>
     <div style="margin: 100px;">
-        <Page :total="100" show-sizer placement="top"></Page>
+        <Page :total="100" :current="val" show-sizer placement="top"></Page>
     </div>
 </template>
 <script>
     export default {
-
+        data () {
+            return {
+                val: 2
+            }
+        }
     }
 </script>
