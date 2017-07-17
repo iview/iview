8b530b1ce3604becf85dfb5728d457ffb0096861
梁灏 <admin@aresn.com>
2017-07-17 17:05:10 +0800

    fixed #1342

--- examples/components/test.vue
+++ examples/components/test.vue
@@ -1,16 +1,40 @@
 <template>
-    <div>{{ row.name }}</div>
+    <Select v-model="model1" style="width:200px">
+        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+    </Select>
 </template>
 <script>
     export default {
-        props: {
-            row: Object
-        },
-        mounted () {
-//            console.log(1);
-        },
-        beforeDestroy () {
-//            console.log(2);
+        data () {
+            return {
+                cityList: [
+                    {
+                        value: 'beijing',
+                        label: '北京市'
+                    },
+                    {
+                        value: 'shanghai',
+                        label: '上海市'
+                    },
+                    {
+                        value: 'shenzhen',
+                        label: '深圳市'
+                    },
+                    {
+                        value: 'hangzhou',
+                        label: '杭州市'
+                    },
+                    {
+                        value: 'nanjing',
+                        label: '南京市'
+                    },
+                    {
+                        value: 'chongqing',
+                        label: '重庆市'
+                    }
+                ],
+                model1: ''
+            }
         }
     }
 </script>
