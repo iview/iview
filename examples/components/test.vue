3e4bab965551fd54537576fff6d7ae8deeb11daa
梁灏 <admin@aresn.com>
2017-07-18 17:58:45 +0800

    Dropdown add transfer prop
    But not support Nested Usage

--- examples/components/test.vue
+++ examples/components/test.vue
@@ -1,7 +1,17 @@
 <template>
-    <Poptip trigger="hover" title="提示标题" :transfer="true" placement="left" content="提示内容">
-        <Button>hover 激活</Button>
-    </Poptip>
+    <Dropdown transfer>
+        <a href="javascript:void(0)">
+            下拉菜单
+            <Icon type="arrow-down-b"></Icon>
+        </a>
+        <Dropdown-menu slot="list">
+            <Dropdown-item>驴打滚</Dropdown-item>
+            <Dropdown-item>炸酱面</Dropdown-item>
+            <Dropdown-item disabled>豆汁儿</Dropdown-item>
+            <Dropdown-item>冰糖葫芦</Dropdown-item>
+            <Dropdown-item divided>北京烤鸭</Dropdown-item>
+        </Dropdown-menu>
+    </Dropdown>
 </template>
 <script>
     export default {
--- examples/routers/dropdown.vue
+++ examples/routers/dropdown.vue
@@ -1,64 +1,29 @@
-<!--<template>-->
-    <!--<Row>-->
-        <!--<i-col span="8">-->
-            <!--<Card>-->
-                <!--<Dropdown @on-click="handleClick">-->
-                    <!--<a href="javascript:void(0)">-->
-                        <!--下拉菜单-->
-                        <!--<Icon type="arrow-down-b"></Icon>-->
-                    <!--</a>-->
-                    <!--<Dropdown-menu slot="list">-->
-                        <!--<Dropdown-item name="1">驴打滚</Dropdown-item>-->
-                        <!--<Dropdown-item name="2">炸酱面</Dropdown-item>-->
-                        <!--<Dropdown-item name="3" disabled>豆汁儿</Dropdown-item>-->
-                        <!--<Dropdown-item name="4">冰糖葫芦</Dropdown-item>-->
-                        <!--<Dropdown-item name="5" divided>北京烤鸭</Dropdown-item>-->
-                    <!--</Dropdown-menu>-->
-                <!--</Dropdown>-->
-            <!--</Card>-->
-        <!--</i-col>-->
-    <!--</Row>-->
-<!--</template>-->
-<!--<script>-->
-    <!--export default {-->
-        <!--methods: {-->
-            <!--handleClick (val) {-->
-                <!--console.log(val)-->
-            <!--}-->
-        <!--}-->
-    <!--}-->
-<!--</script>-->
-
 <template>
-    <Table border :context="self" :columns="columns7" :data="data6"></Table>
+    <Dropdown transfer>
+        <a href="javascript:void(0)">
+            北京小吃
+            <Icon type="arrow-down-b"></Icon>
+        </a>
+        <Dropdown-menu slot="list">
+            <Dropdown-item>驴打滚</Dropdown-item>
+            <Dropdown-item>炸酱面</Dropdown-item>
+            <Dropdown-item>豆汁儿</Dropdown-item>
+            <Dropdown placement="right-start" transfer>
+                <Dropdown-item>
+                    北京烤鸭
+                    <Icon type="ios-arrow-right"></Icon>
+                </Dropdown-item>
+                <Dropdown-menu slot="list">
+                    <Dropdown-item>挂炉烤鸭</Dropdown-item>
+                    <Dropdown-item>焖炉烤鸭</Dropdown-item>
+                </Dropdown-menu>
+            </Dropdown>
+            <Dropdown-item>冰糖葫芦</Dropdown-item>
+        </Dropdown-menu>
+    </Dropdown>
 </template>
 <script>
     export default {
-        data () {
-            return {
-                self: this,
-                columns7: [
-                    {
-                        title: '姓名',
-                        key: 'name',
-                        render (row, column, index) {
-                            return `<Dropdown @on-click="change"><a href="javascript:void(0)">下拉菜单<Icon type="arrow-down-b"></Icon></a><Dropdown-menu slot="list"><Dropdown-item name="1">驴打滚</Dropdown-item><Dropdown-item name="2">炸酱面</Dropdown-item><Dropdown-item disabled name="3">豆汁儿</Dropdown-item><Dropdown-item>冰糖葫芦</Dropdown-item><Dropdown-item divided>北京烤鸭</Dropdown-item></Dropdown-menu></Dropdown>`;
-                        }
-                    }
-                ],
-                data6: [
-                    {
-                        name: '王小明',
-                        age: 18,
-                        address: '北京市朝阳区芍药居'
-                    }
-                ]
-            }
-        },
-        methods: {
-            change (s) {
-                console.log(s)
-            }
-        }
+
     }
 </script>
