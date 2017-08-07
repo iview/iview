63152a8cfee9d70ea6476f9b1b1a4a979800e8cc
fen <guangyaol@olymtech.com>
2017-08-07 20:43:20 +0800

    1.upload 组件增加 remove 的时候 dispatch form 一个 file 对象，以便表单验证的判断 2.upload 组件 success 的时候把 dispatch 放于 onSuccess 之后，使表单验证的时机正常

--- src/components/upload/upload.vue
+++ src/components/upload/upload.vue
@@ -279,8 +279,8 @@
                     _file.status = 'finished';
                     _file.response = res;
 
-                    this.dispatch('FormItem', 'on-form-change', _file);
                     this.onSuccess(res, _file, this.fileList);
+                    this.dispatch('FormItem', 'on-form-change', _file);  // fixed by FEN 先触发导致验证的时候有问题
 
                     setTimeout(() => {
                         _file.showProgress = false;
@@ -301,6 +301,7 @@
                 const fileList = this.fileList;
                 fileList.splice(fileList.indexOf(file), 1);
                 this.onRemove(file, fileList);
+                this.dispatch('FormItem', 'on-form-change', file); // fixed by FEN 删除列表后的验证判断
             },
             handlePreview(file) {
                 if (file.status === 'finished') {
