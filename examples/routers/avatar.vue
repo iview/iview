a1530fac5af6a9bed8791aed1fa5b3201d823b7a
梁灏 <admin@aresn.com>
2017-08-15 11:03:20 +0800

    update Avatar

--- examples/routers/avatar.vue
+++ examples/routers/avatar.vue
@@ -1,5 +1,30 @@
 <template>
-    <Avatar></Avatar>
+    <div>
+        <Avatar icon="person" size="large" style="background-color: #fde3cf;color: #f56a00"></Avatar>
+        <Avatar icon="person"></Avatar>
+        <Avatar icon="person" size="small"></Avatar>
+        <Avatar icon="person" size="large" shape="square"></Avatar>
+        <Avatar icon="person" shape="square"></Avatar>
+        <Avatar icon="person" size="small" shape="square"></Avatar>
+        <br><br>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size="large"></Avatar>
+        <Avatar src="https://avatars2.githubusercontent.com/u/5370542?v=4&s=460"></Avatar>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size="small"></Avatar>
+        <Avatar src="https://avatars2.githubusercontent.com/u/5370542?v=4&s=460" size="large" shape="square"></Avatar>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" shape="square"></Avatar>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size="small" shape="square"></Avatar>
+        <br><br>
+        <Avatar>Aresn</Avatar>
+        <Avatar>U</Avatar>
+        <Avatar>Tomserm</Avatar>
+        <br><br>
+        <Badge dot>
+            <Avatar icon="person" shape="square"></Avatar>
+        </Badge>
+        <Badge :count="3">
+            <Avatar icon="person" shape="square"></Avatar>
+        </Badge>
+    </div>
 </template>
 <script>
     export default {
--- src/components/avatar/avatar.vue
+++ src/components/avatar/avatar.vue
@@ -1,15 +1,38 @@
 <template>
-    <span>
-
+    <span :class="classes">
+        <img :src="src" v-if="src">
+        <span :class="[prefixCls + '-string']" v-else-if="$slots.default"><slot></slot></span>
+        <Icon :type="icon" v-else-if="icon"></Icon>
     </span>
 </template>
 <script>
+    import Icon from '../icon';
+    import { oneOf } from '../../utils/assist';
+
     const prefixCls = 'ivu-avatar';
 
     export default {
         name: 'Avatar',
+        components: { Icon },
         props: {
-
+            shape: {
+                validator (value) {
+                    return oneOf(value, ['circle', 'square']);
+                },
+                default: 'circle'
+            },
+            size: {
+                validator (value) {
+                    return oneOf(value, ['small', 'large', 'default']);
+                },
+                default: 'default'
+            },
+            src: {
+                type: String
+            },
+            icon: {
+                type: String
+            }
         },
         data () {
             return {
@@ -17,7 +40,17 @@
             };
         },
         computed: {
-
+            classes () {
+                return [
+                    `${prefixCls}`,
+                    `${prefixCls}-${this.shape}`,
+                    `${prefixCls}-${this.size}`,
+                    {
+                        [`${prefixCls}-image`]: !!this.src,
+                        [`${prefixCls}-icon`]: !!this.icon
+                    }
+                ];
+            }
         },
         methods: {
 
