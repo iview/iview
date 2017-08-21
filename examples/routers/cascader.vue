f3893b007d801496ded30070f9e21883b012e27e
梁灏 <admin@aresn.com>
2017-08-21 18:04:15 +0800

    update

--- examples/routers/cascader.vue
+++ examples/routers/cascader.vue
@@ -1,61 +1,117 @@
 <template>
-    <Cascader :data="data4" :load-data="loadData"></Cascader>
+    <div style="margin: 100px;width: 200px;">
+        {{ value }}
+        <Cascader v-model="value" :data="data" change-on-select></Cascader>
+        <Button @click="change">change</Button>
+    </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                data4: [
-                    {
-                        value: 'beijing',
-                        label: '北京',
-                        children: [],
-                        loading: false
-                    },
-                    {
-                        value: 'hangzhou',
-                        label: '杭州',
-                        children: [],
-                        loading:false
-                    }
-                ]
+                value: [],
+                data: [{
+                    value: 'beijing',
+                    label: '北京',
+                    children: [
+                        {
+                            value: 'gugong',
+                            label: '故宫'
+                        },
+                        {
+                            value: 'tiantan',
+                            label: '天坛'
+                        },
+                        {
+                            value: 'wangfujing',
+                            label: '王府井'
+                        }
+                    ]
+                }, {
+                    value: 'jiangsu',
+                    label: '江苏',
+                    children: [
+                        {
+                            value: 'nanjing',
+                            label: '南京',
+                            children: [
+                                {
+                                    value: 'fuzimiao',
+                                    label: '夫子庙',
+                                }
+                            ]
+                        },
+                        {
+                            value: 'suzhou',
+                            label: '苏州',
+                            children: [
+                                {
+                                    value: 'zhuozhengyuan',
+                                    label: '拙政园',
+                                },
+                                {
+                                    value: 'shizilin',
+                                    label: '狮子林',
+                                }
+                            ]
+                        }
+                    ],
+                }]
             }
         },
         methods: {
-            loadData (item, callback) {
-                item.loading = true;
-                setTimeout(() => {
-                    console.log(1)
-                    if (item.value === 'beijing') {
-                        item.children = [
-//                            {
-//                                value: 'talkingdata',
-//                                label: 'TalkingData'
-//                            },
-//                            {
-//                                value: 'baidu',
-//                                label: '百度'
-//                            },
-//                            {
-//                                value: 'sina',
-//                                label: '新浪'
-//                            }
-                        ];
-                    } else if (item.value === 'hangzhou') {
-                        item.children = [
-                            {
-                                value: 'ali',
-                                label: '阿里巴巴'
-                            },
-                            {
-                                value: '163',
-                                label: '网易'
-                            }
-                        ];
-                    }
-                    item.loading = false;
-                    callback();
-                }, 1000);
+            change () {
+                this.data = [{
+                    value: 'beijing2',
+                    label: '北京',
+                    children: [
+                        {
+                            value: 'gugong2',
+                            label: '故宫'
+                        },
+                        {
+                            value: 'tiantan2',
+                            label: '天坛'
+                        },
+                        {
+                            value: 'wangfujing2',
+                            label: '王府井'
+                        }
+                    ]
+                }, {
+                    value: 'jiangsu2',
+                    label: '江苏',
+                    children: [
+                        {
+                            value: 'nanjing2',
+                            label: '南京',
+                            children: [
+                                {
+                                    value: 'fuzimiao2',
+                                    label: '夫子庙',
+                                }
+                            ]
+                        },
+                        {
+                            value: 'suzhou2',
+                            label: '苏州',
+                            children: [
+                                {
+                                    value: 'zhuozhengyuan2',
+                                    label: '拙政园',
+                                },
+                                {
+                                    value: 'shizilin2',
+                                    label: '狮子林',
+                                }
+                            ]
+                        }
+                    ],
+                }];
+
+                this.$nextTick(() => {
+                    this.value = ['beijing2', 'tiantan2'];
+                });
             }
         }
     }
