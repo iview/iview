d293cc5ce766879983c5ff778da9d711ba02697d
梁灏 <admin@aresn.com>
2017-09-01 18:04:46 +0800

    fixed #1568 and close #1593

--- examples/routers/tooltip.vue
+++ examples/routers/tooltip.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Tooltip placement="top" transfer content="Tooltip 文字提示" :delay="1000">
+        <Tooltip always placement="top" transfer content="Tooltip 文字提示" :delay="1000">
             <Button @click="disabled = true">延时1秒显示</Button>
         </Tooltip>
         <Tooltip placement="top" transfer content="Tooltip 文字提示">
