0e4166bd0a1694a1735d6738e7c6763fd5d2084d
梁灏 <admin@aresn.com>
2017-09-03 12:02:30 +0800

    fixed #1728

--- examples/routers/cascader.vue
+++ examples/routers/cascader.vue
@@ -1,15 +1,13 @@
 <template>
-    <div style="margin: 100px;width: 200px;">
-        {{ value }}
-        <Cascader v-model="value" :data="data" change-on-select></Cascader>
-        <Button @click="change">change</Button>
+    <div style="width: 200px;margin: 100px;">
+        <Cascader :data="data" v-model="value1"></Cascader>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                value: [],
+                value1: [],
                 data: [{
                     value: 'beijing',
                     label: '北京',
@@ -57,61 +55,6 @@
                         }
                     ],
                 }]
-            }
-        },
-        methods: {
-            change () {
-                this.data = [{
-                    value: 'beijing2',
-                    label: '北京',
-                    children: [
-                        {
-                            value: 'gugong2',
-                            label: '故宫'
-                        },
-                        {
-                            value: 'tiantan2',
-                            label: '天坛'
-                        },
-                        {
-                            value: 'wangfujing2',
-                            label: '王府井'
-                        }
-                    ]
-                }, {
-                    value: 'jiangsu2',
-                    label: '江苏',
-                    children: [
-                        {
-                            value: 'nanjing2',
-                            label: '南京',
-                            children: [
-                                {
-                                    value: 'fuzimiao2',
-                                    label: '夫子庙',
-                                }
-                            ]
-                        },
-                        {
-                            value: 'suzhou2',
-                            label: '苏州',
-                            children: [
-                                {
-                                    value: 'zhuozhengyuan2',
-                                    label: '拙政园',
-                                },
-                                {
-                                    value: 'shizilin2',
-                                    label: '狮子林',
-                                }
-                            ]
-                        }
-                    ],
-                }];
-
-                this.$nextTick(() => {
-                    this.value = ['beijing2', 'tiantan2'];
-                });
             }
         }
     }
