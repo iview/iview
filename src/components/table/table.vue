9ebeb99cd086a2d08aa959a040f9ecabb2b05edd
BarZu <1419328765@qq.com>
2017-09-22 22:13:34 +0800

    将childNodes改为children

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -351,7 +351,7 @@
                         if (allWidth) autoWidthIndex = this.cloneColumns.findIndex(cell => !cell.width);//todo 这行可能有问题
 
                         if (this.data.length) {
-                            const $td = this.$refs.tbody.$el.querySelectorAll('tbody tr')[0].childNodes;
+                            const $td = this.$refs.tbody.$el.querySelectorAll('tbody tr')[0].children;
                             for (let i = 0; i < $td.length; i++) {    // can not use forEach in Firefox
                                 const column = this.cloneColumns[i];
 
