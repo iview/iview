62bf2b3ceb0ad4ead6dfdf78ef6600cdabecc678
daiyanze <ginoalex8964@yahoo.com>
2017-10-18 17:21:52 +0900

    Modify: example of tags

--- examples/routers/tag.vue
+++ examples/routers/tag.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <Tag v-for="item in count" :key="item" :name="item" type="border" color="yellow" @on-close="close" @on-check="check" closable>标签{{ item + 1 }}</Tag>
-        <Tag v-for="item in count" :key="item" :name="item" type="border" color="yellow" @on-close="close" @on-check="check" closable checkable>标签{{ 1 - item }}</Tag>
+        <div><Tag v-for="item in count" :key="item" :name="item" color="yellow" @on-close="close" @on-check="check" checkable>可选标签{{ item + 1 }}</Tag></div>
+        <div><Tag v-for="item in count" :key="item" :name="item" type="dot" color="blue" @on-close="close" @on-check="check" closable checkable>可选可关闭标签{{ item + 1 }}</Tag></div>
+        <br />
         <Button icon="ios-plus-empty" type="dashed" size="small" @click="count += 1">添加标签</Button>
     </div>
 </template>
