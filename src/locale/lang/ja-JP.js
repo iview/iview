5064c7b3febcc85e6861f7f133b4e733b8599b15
梁灏 <admin@aresn.com>
2017-10-23 16:52:30 +0800

    update ja-JP

--- src/locale/lang/ja-JP.js
+++ src/locale/lang/ja-JP.js
@@ -20,7 +20,7 @@ export default {
             endTime: '終了時間',
             clear: 'クリーア',
             ok: '確定',
-            datePanelLabel: '[yyyy] [mmmm]',
+            datePanelLabel: '[yyyy年] [m月]',
             month: '月',
             month1: '1 月',
             month2: '2 月',
