3d2d64b0e5c49bd591abec29149bac520f6327a9
yangd <13532023084@139.com>
2017-11-04 14:35:42 +0800

    iview/indexc.d.ts中组件名称排序

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,134 +6,127 @@ import Vue from "vue";
 
 export default IView;
 
-import { GridRow, GridCol } from "./grid";
+import { Affix } from "./affix";
+import { Alert } from "./alert";
+import { AutoComplete } from "./auto-complete";
+import { Avatar } from "./avatar";
+import { BackTop } from "./back-top";
+import { Badge } from "./badge";
+import { Breadcrumb, BreadcrumbItem } from "./breadcrumb";
 import { Button, ButtonGroup } from "./button";
+import { Card } from "./card";
+import { Carousel } from "./carousel";
+import { Cascader } from "./cascader";
+import { Checkbox, CheckboxGroup } from "./checkbox";
+import { Circle } from "./circle";
+import { Collapse, CollapsePanel } from "./collapse";
+import { ColorPicker } from "./color-picker";
+import { DatePicker, DatePickerOptions } from "./date-picker";
+import { Dropdown, DropdownItem } from "./dropdown";
+import { Form, FormItem } from "./form";
+import { GridRow, GridCol } from "./grid";
 import { Icon } from "./icon";
 import { Input } from "./input";
-import { Radio, RadioGroup } from "./radio";
-import { Checkbox, CheckboxGroup } from "./checkbox";
-import { Switch } from "./switch";
-import {
-  Table,
-  TableColumn,
-  TableRenderCreateElementData,
-  TableRenderCreateElementResult,
-  TableColumnRenderParams,
-  TableExportCsvParams
-} from "./table";
-import { Tabs, TabPane } from "./tabs";
-import { Select, SelectOption, SelectOptionGroup } from "./select";
-import { AutoComplete } from "./auto-complete";
-import { DatePicker, DatePickerOptions } from "./date-picker";
-import { TimePicker } from "./time-picker";
-import { Cascader } from "./cascader";
-import { Transfer } from "./transfer";
 import { InputNumber } from "./input-number";
-import { Rate } from "./rate";
-import { Upload } from "./upload";
-import { ColorPicker } from "./color-picker";
-import { Form, FormItem } from "./form";
-import { Alert } from "./alert";
-import { Card } from "./card";
-import { MessageInstance, MessageConfig } from "./message";
-import { NoticeInstance, NoticeConfig, NoticeGlobalConfig } from "./notice";
-import { Modal, ModalInstance, ModalConfig } from "./modal";
-import { Progress } from "./progress";
-import { Avatar } from "./avatar";
-import { Badge } from "./badge";
-import { Collapse, CollapsePanel } from "./collapse";
-import { Timeline, TimelineItem } from "./timeline";
-import { Tag } from "./tag";
-import { Tooltip } from "./tooltip";
-import { Poptip } from "./poptip";
-import { Carousel } from "./carousel";
-import { Tree,TreeChildren } from "./tree";
-import { Menu, MenuItem, Submenu, MenuGroup } from "./menu";
-import { Dropdown, DropdownItem } from "./dropdown";
-import { Page } from "./page";
-import { Breadcrumb, BreadcrumbItem } from "./breadcrumb";
-import { Steps, Step } from "./steps";
 import { LoadingBarInstance, LoadingBarConfig } from "./loading-bar";
-import { Circle } from "./circle";
-import { Affix } from "./affix";
-import { BackTop } from "./back-top";
-import { Spin } from "./spin";
+import { Menu, MenuItem, Submenu, MenuGroup } from "./menu";
+import { MessageInstance, MessageConfig } from "./message";
+import { Modal, ModalInstance, ModalConfig } from "./modal";
+import { NoticeInstance, NoticeConfig, NoticeGlobalConfig } from "./notice";
+import { Page } from "./page";
+import { Poptip } from "./poptip";
+import { Progress } from "./progress";
+import { Radio, RadioGroup } from "./radio";
+import { Rate } from "./rate";
 import { Scroll } from "./scroll";
+import { Select, SelectOption, SelectOptionGroup } from "./select";
+import { Spin } from "./spin";
+import { Steps, Step } from "./steps";
+import { Switch } from "./switch";
+import { Table, TableColumn, TableRenderCreateElementData, TableRenderCreateElementResult, TableColumnRenderParams, TableExportCsvParams } from "./table";
+import { Tabs, TabPane } from "./tabs";
+import { Tag } from "./tag";
+import { Timeline, TimelineItem } from "./timeline";
+import { TimePicker } from "./time-picker";
+import { Tooltip } from "./tooltip";
+import { Transfer } from "./transfer";
+import { Tree, TreeChildren } from "./tree";
+import { Upload } from "./upload";
 
 export declare module IView {
-  interface IGridRow extends GridRow { }
-  interface IGridCol extends GridCol { }
+  interface IAffix extends Affix { }
+  interface IAlert extends Alert { }
+  interface IAutoComplete extends AutoComplete { }
+  interface IAvatar extends Avatar { }
+  interface IBackTop extends BackTop { }
+  interface IBadge extends Badge { }
+  interface IBreadcrumb extends Breadcrumb { }
+  interface IBreadcrumbItem extends BreadcrumbItem { }
   interface IButton extends Button { }
   interface IButtonGroup extends ButtonGroup { }
-  interface IIcon extends Icon { }
-  interface IInput extends Input { }
-  interface IRadio extends Radio { }
-  interface IRadioGroup extends RadioGroup { }
+  interface ICard extends Card { }
+  interface ICarousel extends Carousel { }
+  interface ICascader extends Cascader { }
   interface ICheckbox extends Checkbox { }
   interface ICheckboxGroup extends CheckboxGroup { }
-  interface ISwitch extends Switch { }
-  interface ITable extends Table { }
-  interface ITableColumn extends TableColumn { }
-  interface ITableRenderCreateElementData extends TableRenderCreateElementData { }
-  interface ITableRenderCreateElementResult extends TableRenderCreateElementResult { }
-  interface ITableColumnRenderParams extends TableColumnRenderParams { }
-  interface ITableExportCsvParams extends TableExportCsvParams { }
-  interface ITabs extends Tabs { }
-  interface ITabPane extends TabPane { }
+  interface ICircle extends Circle { }
+  interface ICollapse extends Collapse { }
+  interface ICollapsePanel extends CollapsePanel { }
+  interface IColorPicker extends ColorPicker { }
+  interface IDatePicker extends DatePicker { }
+  interface IDatePickerOptions extends DatePickerOptions { }
+  interface IDropdown extends Dropdown { }
+  interface IDropdownItem extends DropdownItem { }
+  interface IForm extends Form { }
+  interface IFormItem extends FormItem { }
+  interface IGridCol extends GridCol { }
+  interface IGridRow extends GridRow { }
+  interface IIcon extends Icon { }
+  interface IInput extends Input { }
+  interface IInputNumber extends InputNumber { }
+  interface ILoadingBarConfig extends LoadingBarConfig { }
+  interface ILoadingBarInstance extends LoadingBarInstance { }
+  interface IMenu extends Menu { }
+  interface IMenuGroup extends MenuGroup { }
+  interface IMenuItem extends MenuItem { }
+  interface IMessageConfig extends MessageConfig { }
+  interface IMessageInstance extends MessageInstance { }
+  interface IModal extends Modal { }
+  interface IModalConfig extends ModalConfig { }
+  interface IModalInstance extends ModalInstance { }
+  interface INoticeConfig extends NoticeConfig { }
+  interface INoticeGlobalConfig extends NoticeGlobalConfig { }
+  interface INoticeInstance extends NoticeInstance { }
+  interface IPage extends Page { }
+  interface IPoptip extends Poptip { }
+  interface IProgress extends Progress { }
+  interface IRadio extends Radio { }
+  interface IRadioGroup extends RadioGroup { }
+  interface IRate extends Rate { }
+  interface IScroll extends Scroll { }
   interface ISelect extends Select { }
   interface ISelectOption extends SelectOption { }
   interface ISelectOptionGroup extends SelectOptionGroup { }
-  interface IAutoComplete extends AutoComplete { }
-  interface IDatePicker extends DatePicker { }
-  interface IDatePickerOptions extends DatePickerOptions { }
-  interface ITimePicker extends TimePicker { }
-  interface ICascader extends Cascader { }
-  interface ITransfer extends Transfer { }
-  interface IInputNumber extends InputNumber { }
-  interface IRate extends Rate { }
-  interface IUpload extends Upload { }
-  interface IColorPicker extends ColorPicker { }
-  interface IForm extends Form { }
-  interface IFormItem extends FormItem { }
-  interface IAlert extends Alert { }
-  interface ICard extends Card { }
-  interface IMessageInstance extends MessageInstance { }
-  interface IMessageConfig extends MessageConfig { }
-  interface INoticeInstance extends NoticeInstance { }
-  interface INoticeConfig extends NoticeConfig { }
-  interface INoticeGlobalConfig extends NoticeGlobalConfig { }
-  interface IModal extends Modal { }
-  interface IModalInstance extends ModalInstance { }
-  interface IModalConfig extends ModalConfig { }
-  interface IProgress extends Progress { }
-  interface IAvatar extends Avatar { }
-  interface IBadge extends Badge { }
-  interface ICollapse extends Collapse { }
-  interface ICollapsePanel extends CollapsePanel { }
+  interface ISpin extends Spin { }
+  interface IStep extends Step { }
+  interface ISteps extends Steps { }
+  interface ISubmenu extends Submenu { }
+  interface ISwitch extends Switch { }
+  interface ITable extends Table { }
+  interface ITableColumn extends TableColumn { }
+  interface ITableColumnRenderParams extends TableColumnRenderParams { }
+  interface ITableExportCsvParams extends TableExportCsvParams { }
+  interface ITableRenderCreateElementData extends TableRenderCreateElementData { }
+  interface ITableRenderCreateElementResult extends TableRenderCreateElementResult { }
+  interface ITabPane extends TabPane { }
+  interface ITabs extends Tabs { }
+  interface ITag extends Tag { }
   interface ITimeline extends Timeline { }
   interface ITimelineItem extends TimelineItem { }
-  interface ITag extends Tag { }
+  interface ITimePicker extends TimePicker { }
   interface ITooltip extends Tooltip { }
-  interface IPoptip extends Poptip { }
-  interface ICarousel extends Carousel { }
+  interface ITransfer extends Transfer { }
   interface ITree extends Tree { }
   interface ITreeChildren extends TreeChildren { }
-  interface IMenu extends Menu { }
-  interface IMenuItem extends MenuItem { }
-  interface ISubmenu extends Submenu { }
-  interface IMenuGroup extends MenuGroup { }
-  interface IDropdown extends Dropdown { }
-  interface IDropdownItem extends DropdownItem { }
-  interface IPage extends Page { }
-  interface IBreadcrumb extends Breadcrumb { }
-  interface IBreadcrumbItem extends BreadcrumbItem { }
-  interface ISteps extends Steps { }
-  interface IStep extends Step { }
-  interface ILoadingBarInstance extends LoadingBarInstance { }
-  interface ILoadingBarConfig extends LoadingBarConfig { }
-  interface ICircle extends Circle { }
-  interface IAffix extends Affix { }
-  interface IBackTop extends BackTop { }
-  interface ISpin extends Spin { }
-  interface IScroll extends Scroll { }
+  interface IUpload extends Upload { }
 }
