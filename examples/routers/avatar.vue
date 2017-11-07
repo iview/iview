84a4a95dbfc2cf22a537a30473fc443094c5a487
梁灏 <admin@aresn.com>
2017-11-07 17:36:26 +0800

    fixed #1926

--- examples/routers/avatar.vue
+++ examples/routers/avatar.vue
@@ -29,13 +29,47 @@
             <Avatar icon="person" shape="square"></Avatar>
         </Badge>
         <Button @click="change">change</Button>
+
+        <Menu mode="horizontal" :theme="theme1" active-name="1">
+            <MenuItem name="1">
+                <Icon type="ios-paper"></Icon>
+                内容管理
+            </MenuItem>
+            <MenuItem name="2">
+                <Icon type="ios-people"></Icon>
+                用户管理
+            </MenuItem>
+            <Submenu name="3">
+                <template slot="title">
+                    <Icon type="stats-bars"></Icon>
+                    统计分析
+                </template>
+                <MenuGroup title="使用">
+                    <MenuItem name="3-1">新增和启动</MenuItem>
+                    <MenuItem name="3-2">活跃分析</MenuItem>
+                    <MenuItem name="3-3">时段分析</MenuItem>
+                </MenuGroup>
+                <MenuGroup title="留存">
+                    <MenuItem name="3-4">用户留存</MenuItem>
+                    <MenuItem name="3-5">流失用户</MenuItem>
+                </MenuGroup>
+            </Submenu>
+            <MenuItem name="4">
+                <Icon type="settings"></Icon>
+                综合设置
+            </MenuItem>
+            <div>
+                <Avatar icon="person"></Avatar>
+            </div>
+        </Menu>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                name: 'Aresn'
+                name: 'Aresn',
+                theme1: 'dark'
             }
         },
         methods: {
