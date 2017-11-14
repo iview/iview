7b0fb4ef9bb35fbfdb882cc961f4585c8bcae92f
梁灏 <admin@aresn.com>
2017-11-14 09:31:48 +0800

    release 2.7.1

--- src/index.js
+++ src/index.js
@@ -153,7 +153,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 const API = {
-    version: '2.7.0',
+    version: '2.7.1',
     locale: locale.use,
     i18n: locale.i18n,
     install,
