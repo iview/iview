450e18ef877fb7a92cc2f623e6762d342ca7cc79
zhigang.li <zhigang.li@tendcloud.com>
2017-12-01 21:10:11 +0800

    make tag suppor custom colors

--- examples/routers/tag.vue
+++ examples/routers/tag.vue
@@ -14,6 +14,11 @@
         <Tag closable color="red" checkable>标签三</Tag>
         <Tag closable color="yellow" checkable>标签四</Tag>
         <br><br>
+        <Tag closable color="#EF6AFF" checkable>标签一</Tag>
+        <Tag type="border" closable color="#EF6AFF" checkable>标签二</Tag>
+        <Tag type="dot" closable color="#EF6AFF" checkable>标签三</Tag>
+        <Tag closable color="default" checkable>标签四</Tag>
+        <br><br>
         <Tag type="border" closable color="blue" checkable>标签一</Tag>
         <Tag type="border" closable color="green">标签二</Tag>
         <Tag type="border" closable color="red">标签三</Tag>
