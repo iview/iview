5b3c1784169b8d8ecc920137fbbbb5d974cdd23e
Sergio Crisostomo <sergiosbox@gmail.com>
2017-12-12 15:52:24 +0100

    refactor locale to not depend on Vue

--- src/locale/format.js
+++ src/locale/format.js
@@ -6,45 +6,40 @@
 
 const RE_NARGS = /(%|)\{([0-9a-zA-Z_]+)\}/g;
 
-export default function() {
-    // const { hasOwn } = Vue.util;
-    function hasOwn (obj, key) {
-        return Object.prototype.hasOwnProperty.call(obj, key);
+function hasOwn (obj, key) {
+    return Object.prototype.hasOwnProperty.call(obj, key);
+}
+
+/**
+ * template
+ *
+ * @param {String} string
+ * @param {Array} ...args
+ * @return {String}
+ */
+
+export default function template(string, ...args) {
+    if (args.length === 1 && typeof args[0] === 'object') {
+        args = args[0];
     }
 
-    /**
-     * template
-     *
-     * @param {String} string
-     * @param {Array} ...args
-     * @return {String}
-     */
+    if (!args || !args.hasOwnProperty) {
+        args = {};
+    }
 
-    function template(string, ...args) {
-        if (args.length === 1 && typeof args[0] === 'object') {
-            args = args[0];
-        }
+    return string.replace(RE_NARGS, (match, prefix, i, index) => {
+        let result;
 
-        if (!args || !args.hasOwnProperty) {
-            args = {};
-        }
-
-        return string.replace(RE_NARGS, (match, prefix, i, index) => {
-            let result;
-
-            if (string[index - 1] === '{' &&
-                string[index + match.length] === '}') {
-                return i;
-            } else {
-                result = hasOwn(args, i) ? args[i] : null;
-                if (result === null || result === undefined) {
-                    return '';
-                }
-
-                return result;
+        if (string[index - 1] === '{' &&
+            string[index + match.length] === '}') {
+            return i;
+        } else {
+            result = hasOwn(args, i) ? args[i] : null;
+            if (result === null || result === undefined) {
+                return '';
             }
-        });
-    }
 
-    return template;
+            return result;
+        }
+    });
 }
--- src/locale/index.js
+++ src/locale/index.js
@@ -1,21 +1,10 @@
 import defaultLang from './lang/zh-CN';
-import Vue from 'vue';
-import deepmerge from 'deepmerge';
-import Format from './format';
+import format from './format';
 
-const format = Format(Vue);
 let lang = defaultLang;
-let merged = false;
 let i18nHandler = function() {
-    const vuei18n = Object.getPrototypeOf(this || Vue).$t;
-    if (typeof vuei18n === 'function' && !!Vue.locale) {
-        if (!merged) {
-            merged = true;
-            Vue.locale(
-                Vue.config.lang,
-                deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
-            );
-        }
+    const vuei18n = Object.getPrototypeOf(this).$t;
+    if (typeof vuei18n === 'function') {
         return vuei18n.apply(this, arguments);
     }
 };
