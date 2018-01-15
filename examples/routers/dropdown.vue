3288b530999f14e30e29c9c5f6cfb5b62d52b3a2
梁灏 <admin@aresn.com>
2018-01-15 18:11:41 +0800

    update

--- examples/routers/dropdown.vue
+++ examples/routers/dropdown.vue
@@ -1,25 +1,16 @@
 <template>
-    <Dropdown transfer>
+    <Dropdown trigger="click" style="margin-left: 20px">
         <a href="javascript:void(0)">
-            北京小吃
+            click 触发
             <Icon type="arrow-down-b"></Icon>
         </a>
-        <Dropdown-menu slot="list">
-            <Dropdown-item>驴打滚</Dropdown-item>
-            <Dropdown-item>炸酱面</Dropdown-item>
-            <Dropdown-item>豆汁儿</Dropdown-item>
-            <Dropdown placement="right-start" transfer>
-                <Dropdown-item>
-                    北京烤鸭
-                    <Icon type="ios-arrow-right"></Icon>
-                </Dropdown-item>
-                <Dropdown-menu slot="list">
-                    <Dropdown-item>挂炉烤鸭</Dropdown-item>
-                    <Dropdown-item>焖炉烤鸭</Dropdown-item>
-                </Dropdown-menu>
-            </Dropdown>
-            <Dropdown-item>冰糖葫芦</Dropdown-item>
-        </Dropdown-menu>
+        <DropdownMenu slot="list">
+            <DropdownItem>驴打滚</DropdownItem>
+            <DropdownItem>炸酱面</DropdownItem>
+            <DropdownItem>豆汁儿</DropdownItem>
+            <DropdownItem>冰糖葫芦</DropdownItem>
+            <DropdownItem>北京烤鸭</DropdownItem>
+        </DropdownMenu>
     </Dropdown>
 </template>
 <script>
