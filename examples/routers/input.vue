076148112f937f1c15c2930539cfe47891361e0a
梁灏 <admin@aresn.com>
2018-01-23 10:14:56 +0800

    update Input textarea style

--- examples/routers/input.vue
+++ examples/routers/input.vue
@@ -1,30 +1,15 @@
 <template>
-    <div style="width: 100px;">
-        <Input autocomplete="on" v-model="value1" size="large" placeholder="large size"></Input>
-        <br>
-        <Input v-model="value2" placeholder="default size"></Input>
-        <br>
-        <Input v-model="value3" size="small" placeholder="small size"></Input>
-        <br>
-        <Input v-model="value1" size="large" placeholder="large size" icon="ios-clock-outline"></Input>
-        <br>
-        <Input v-model="value2" placeholder="default size" icon="ios-clock-outline"></Input>
-        <br>
-        <Input v-model="value3" size="small" placeholder="small size" icon="ios-clock-outline"></Input>
-        <br>
-        <Input v-model="obj.test" type="textarea" autosize></Input>
+    <div style="width: 300px">
+        <Input v-model="value7" type="textarea" :autosize="true" placeholder="Enter something..."></Input>
+        <Input v-model="value8" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                value1: '',
-                value2: '',
-                value3: '',
-                obj: {
-
-                }
+                value7: '',
+                value8: ''
             }
         }
     }
