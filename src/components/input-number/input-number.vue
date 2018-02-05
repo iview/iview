ec7768d0fadd9cc7f8f5451f50d8c8a5186ffd64
daiyanze <ginoalex8964@yahoo.com>
2018-02-05 19:36:33 +0900

    Fix: NaN flashed out problem

--- src/components/input-number/input-number.vue
+++ src/components/input-number/input-number.vue
@@ -271,16 +271,13 @@
                 const isEmptyString = val.length === 0;
 
                 if (event.type == 'input') {
-                    if (this.empty) {
+                    this.currentValue = this.min;
+                    this.setValue(this.currentValue);
+                    if (isEmptyString) {
                         // If input is cleared out, set currentValue NaN
-                        if (isEmptyString) {
-                            this.currentValue = NaN;
-                            this.setValue(NaN);
-                            return;
-                        }
-                        // set value to min if invalid keyboard interupts
-                        this.currentValue = this.min;
-                        this.setValue(this.min);
+                        this.currentValue = NaN;
+                        this.setValue(this.currentValue);
+                        return;
                     }
                     if (val.match(/^\-?\.?$|\.$/)) return; // prevent fire early if decimal. If no more input the change event will fire later
                 }
