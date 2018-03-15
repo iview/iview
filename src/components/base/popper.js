3684e716eba6cff8eea011cad4c6287e05e1f74e
Aresn <admin@aresn.com>
2018-03-15 10:42:44 +0800

    SSR support

--- src/components/base/popper.js
+++ src/components/base/popper.js
@@ -3,8 +3,7 @@
  * */
 import Vue from 'vue';
 const isServer = Vue.prototype.$isServer;
-// const Popper = isServer ? function() {} : require('popper.js');  // eslint-disable-line
-import Popper from 'popper.js';
+const Popper = isServer ? function() {} : require('popper.js/dist/umd/popper.js');  // eslint-disable-line
 
 export default {
     props: {
