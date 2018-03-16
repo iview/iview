10d29acb9720c3a833f8e9ff031df47f304eb1d5
huanghong <huanghong1125@126.com>
2018-03-16 13:59:59 +0800

    InputNumber support null values

--- examples/routers/input-number.vue
+++ examples/routers/input-number.vue
@@ -43,6 +43,11 @@
     <div>
         <InputNumber :max="1000000000" :min="1" v-model="value1" :formatter="formatter" :parser="parser" @on-change="change" style="width: 200px"></InputNumber>
         <InputNumber :max="1000000000" :min="1" v-model="value2" :formatter="formatter2" :parser="parser2" @on-change="change" style="width: 200px"></InputNumber>
+        
+        <InputNumber @on-change="change" style="width: 200px"></InputNumber>
+        
+        <InputNumber v-model="valueNull" @on-change="change" style="width: 200px"></InputNumber>
+        <InputNumber v-model="valueNull" @on-change="change" :formatter="formatter" :parser="parser" style="width: 200px"></InputNumber>
     </div>
 </template>
 <script>
@@ -51,6 +56,7 @@
             return {
                 value1: 1800000,
                 value2: 55,
+                valueNull:null,
                 formatter: (value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ','),
                 parser: (value) => value.replace(/\$\s?|(,*)/g, ''),
                 formatter2: (value) => `${value}%`,
