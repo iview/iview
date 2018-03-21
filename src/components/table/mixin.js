e399c5e9072ac1416ce83ce1b5cb49f285cdaaea
huanghong <huanghong1125@126.com>
2018-03-21 15:27:14 +0800

    remove console.log

--- src/components/table/mixin.js
+++ src/components/table/mixin.js
@@ -25,9 +25,6 @@ export default {
                 width = this.columnsWidth[column._index].width;
             }
             // when browser has scrollBar,set a width to resolve scroll position bug
-            if(this.columns.length==7 && column.fixed){
-                console.log(column);
-            }
             if (width && this.columns.length === index + 1 && top && this.$parent.bodyHeight !== 0 && column.fixed!=='left' && !this.fixed) {
                 let scrollBarWidth = this.$parent.scrollBarWidth;
                 if (!this.$parent.showVerticalScrollBar) scrollBarWidth = 0;
