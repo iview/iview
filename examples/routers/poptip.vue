dee03ef3a19d956490a956dff0f889bc315c948d
梁灏 <admin@aresn.com>
2018-04-10 12:16:05 +0800

    update Poptip demo

--- examples/routers/poptip.vue
+++ examples/routers/poptip.vue
@@ -1,21 +1,28 @@
 <template>
-    <div style="margin: 200px;">
-        <Poptip title="提示标题" transfer>
-            <div slot="content" style="padding: 50px">
-                <Button>click me</Button>
-            </div>
-            <Button>click 激活</Button>
-        </Poptip>
+    <div style="margin: 200px">
+        <Tabs value="name1">
+            <TabPane label="标签一" name="name1">
+                <div style="overflow: auto;width: 200px;height:300px">
+                    <Poptip title="提示标题" content="标签一的内容" placement="right" transfer :options="options">
+                        <Button id="aaa">右边</Button>
+                    </Poptip>
+                </div>
+            </TabPane>
+            <TabPane label="标签二" name="name2">标签二的内容</TabPane>
+        </Tabs>
     </div>
 </template>
 <script>
     export default {
-        methods: {
-            ok () {
-                this.$Message.info('点击了确定');
-            },
-            cancel () {
-                this.$Message.info('点击了取消');
+        data () {
+            return {
+                options: {
+                    modifiers: {
+                        preventOverflow: {
+                            boundariesElement: 'body',
+                        }
+                    }
+                }
             }
         }
     }
