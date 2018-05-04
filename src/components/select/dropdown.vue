8c82b6f27bd173f285ba8bc67a3242d44d09a398
梁灏 <admin@aresn.com>
2018-05-04 11:00:15 +0800

    update Select

--- src/components/select/dropdown.vue
+++ src/components/select/dropdown.vue
@@ -79,6 +79,9 @@
                 }
             },
             resetTransformOrigin() {
+                // 不判断，Select 会报错，不知道为什么
+                if (!this.popper) return;
+
                 let x_placement = this.popper.popper.getAttribute('x-placement');
                 let placementStart = x_placement.split('-')[0];
                 let placementEnd = x_placement.split('-')[1];
