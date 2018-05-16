4c682092e3327c480027ecf6a0e9de1b60166fab
zhigang.li <zhigang.li@tendcloud.com>
2018-05-16 10:38:22 +0800

    udpate

--- src/utils/assist.js
+++ src/utils/assist.js
@@ -234,9 +234,7 @@ export function findBrothersComponents (context, componentName, exceptMe = true)
     let res = context.$parent.$children.filter(item => {
         return item.$options.name === componentName;
     });
-    let index = res.findIndex(item => {
-        return item._uid === context._uid;
-    });
+    let index = res.findIndex(item => item._uid === context._uid);
     if (exceptMe) res.splice(index, 1);
     return res;
 }
