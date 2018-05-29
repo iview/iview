d20b4e1a73a05029a5125bb320a6e4a062b6552e
Sergio Crisostomo <sergiosbox@gmail.com>
2018-05-29 15:06:50 +0200

    detect touched so touch devices can also be selected

--- src/components/select/option.vue
+++ src/components/select/option.vue
@@ -2,6 +2,7 @@
     <li
         :class="classes"
         @click.stop="select"
+        @touchend.stop="select"
         @mousedown.prevent
         @touchstart.prevent
     ><slot>{{ showLabel }}</slot></li>
