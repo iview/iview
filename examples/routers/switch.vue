ac2f8493b1f79c5faaabddf8cba63832a91a3bee
梁灏 <admin@aresn.com>
2018-06-21 19:51:38 +0800

    update Switch style

--- examples/routers/switch.vue
+++ examples/routers/switch.vue
@@ -6,13 +6,39 @@
         </i-switch>
         {{ m1 }}
         <div @click="m1 = 'no'">toggle</div>
+        <br><br>
+        <i-switch size="large"></i-switch>
+        <i-switch></i-switch>
+        <i-switch size="small"></i-switch>
+        <br><br>
+        <i-switch>
+            <span slot="open">开</span>
+            <span slot="close">关</span>
+        </i-switch>
+        <i-switch>
+            <Icon type="android-done" slot="open"></Icon>
+            <Icon type="android-close" slot="close"></Icon>
+        </i-switch>
+        <br><br>
+        <i-switch size="large">
+            <span slot="open">开启</span>
+            <span slot="close">关闭</span>
+        </i-switch>
+        <i-switch size="large">
+            <span slot="open">ON</span>
+            <span slot="close">OFF</span>
+        </i-switch>
+        <br><br>
+        <i-switch :disabled="disabled"></i-switch>
+        <Button type="primary" @click="disabled = !disabled">Toggle Disabled</Button>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                m1: 'yes'
+                m1: 'yes',
+                disabled: true
             }
         },
         methods: {
