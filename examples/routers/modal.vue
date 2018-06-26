9edded493134638b22d1df09a9f83c482a34b17e
梁灏 <admin@aresn.com>
2018-06-26 21:01:56 +0800

    update Modal demo

--- examples/routers/modal.vue
+++ examples/routers/modal.vue
@@ -1,10 +1,21 @@
 <template>
     <div>
         <Button type="primary" @click="modal1 = true">Display dialog box</Button>
+        <Button type="primary" @click="modal2 = true">Display dialog box</Button>
         <Button @click="hc">Click Me</Button>
         <Modal
                 v-model="modal1"
                 title="Common Modal dialog box title"
+                dragable
+                @on-ok="ok"
+                @on-cancel="cancel">
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+            <p>Content of dialog</p>
+        </Modal>
+        <Modal
+                v-model="modal2"
+                title="Common Modal dialog box title2"
                 dragable
                 @on-ok="ok"
                 @on-cancel="cancel">
@@ -18,7 +29,8 @@
     export default {
         data () {
             return {
-                modal1: false
+                modal1: false,
+                modal2: false
             }
         },
         methods: {
