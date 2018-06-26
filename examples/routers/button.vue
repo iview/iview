877c7fd8708c29ebec75f24ffcb6955b5e2af428
梁灏 <admin@aresn.com>
2018-06-26 17:07:15 +0800

    update Button disabled color

--- examples/routers/button.vue
+++ examples/routers/button.vue
@@ -9,7 +9,16 @@
         <Button type="error">Error</Button>
         <Button type="text">Text</Button>
         <br><br><br>
-        <div style="padding: 20px;background: #dddee1">
+        <Button type="default" disabled>Default</Button>
+        <Button type="primary" disabled>Primary</Button>
+        <Button type="dashed" disabled>Dashed</Button>
+        <Button type="info" disabled>Info</Button>
+        <Button type="success" disabled>Success</Button>
+        <Button type="warning" disabled>Warning</Button>
+        <Button type="error" disabled>Error</Button>
+        <Button type="text" disabled>Text</Button>
+        <br><br><br>
+        <div style="padding: 20px;background: rgb(190, 200, 200)">
             <Button type="default" ghost>Default</Button>
             <Button type="primary" ghost>Primary</Button>
             <Button type="dashed" ghost>Dashed</Button>
@@ -18,6 +27,15 @@
             <Button type="warning" ghost>Warning</Button>
             <Button type="error" ghost>Error</Button>
             <Button type="text" ghost>Text</Button>
+            <br><br><br>
+            <Button type="default" ghost disabled>Default</Button>
+            <Button type="primary" ghost disabled>Primary</Button>
+            <Button type="dashed" ghost disabled>Dashed</Button>
+            <Button type="info" ghost disabled>Info</Button>
+            <Button type="success" ghost disabled>Success</Button>
+            <Button type="warning" ghost disabled>Warning</Button>
+            <Button type="error" ghost disabled>Error</Button>
+            <Button type="text" ghost disabled>Text</Button>
         </div>
         <br><br><br>
         <Button type="primary" icon="ios-information-circle" size="small">Search</Button>
