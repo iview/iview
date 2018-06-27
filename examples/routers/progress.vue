724a607712a6184bb25bf2cce6fda47dcc324323
梁灏 <admin@aresn.com>
2018-06-27 17:49:00 +0800

    update Progress style

--- examples/routers/progress.vue
+++ examples/routers/progress.vue
@@ -5,6 +5,12 @@
         <Progress vertical :percent="65" status="wrong"></Progress>
         <Progress vertical :percent="100"></Progress>
         <Progress vertical :percent="25" hide-info></Progress>
+
+        <Progress  :percent="25"></Progress>
+        <Progress  :percent="45" status="active"></Progress>
+        <Progress  :percent="65" status="wrong"></Progress>
+        <Progress  :percent="100"></Progress>
+        <Progress  :percent="25" hide-info></Progress>
     </div>
 </template>
 <script>
