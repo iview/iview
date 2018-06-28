22111588976de090bec759ff53d68323bf223255
梁灏 <admin@aresn.com>
2018-06-28 14:23:51 +0800

    Switch support global setting

--- src/components/switch/switch.vue
+++ src/components/switch/switch.vue
@@ -41,6 +41,9 @@
             size: {
                 validator (value) {
                     return oneOf(value, ['large', 'small', 'default']);
+                },
+                default () {
+                    return this.$IVIEW.size === '' ? 'default' : this.$IVIEW.size;
                 }
             },
             name: {
