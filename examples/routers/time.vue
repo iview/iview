2b98434e90077cf9c5c3fa7ca089cbea69b7ae7d
梁灏 <admin@aresn.com>
2018-06-29 15:15:49 +0800

    update Time

--- examples/routers/time.vue
+++ examples/routers/time.vue
@@ -3,13 +3,15 @@
         <Time :time="1526608921" />
         <Time :time="1652839997" />
         <Time :time="ddd" :interval="1" />
+
+
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                ddd: new Date('2018-04-27 14:23:00')
+                ddd: new Date()
             };
         },
         computed: {},
