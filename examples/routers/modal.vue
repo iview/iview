1d247d20997948ea837361952647e9d42db3e6e1
梁灏 <admin@aresn.com>
2018-07-01 14:43:25 +0800

    update Modal & Notice style

--- examples/routers/modal.vue
+++ examples/routers/modal.vue
@@ -1,30 +1,43 @@
 <template>
     <div>
-        <Button type="primary" @click="modal1 = true">Display dialog box</Button>
-        <Modal
-                v-model="modal1"
-                title="Common Modal dialog box title"
-                @on-ok="ok"
-                @on-cancel="cancel">
-            <p>Content of dialog</p>
-            <p>Content of dialog</p>
-            <p>Content of dialog</p>
-        </Modal>
+        <Button @click="instance('info')">Info</Button>
+        <Button @click="instance('success')">Success</Button>
+        <Button @click="instance('warning')">Warning</Button>
+        <Button @click="instance('error')">Error</Button>
     </div>
 </template>
 <script>
     export default {
-        data () {
-            return {
-                modal1: false
-            }
-        },
         methods: {
-            ok () {
-                this.$Message.info('Clicked ok');
-            },
-            cancel () {
-                this.$Message.info('Clicked cancel');
+            instance (type) {
+                const title = 'Title';
+                const content = '<p>Content of dialog</p><p>Content of dialog</p>';
+                switch (type) {
+                    case 'info':
+                        this.$Modal.info({
+                            title: title,
+                            content: content
+                        });
+                        break;
+                    case 'success':
+                        this.$Modal.success({
+                            title: title,
+                            content: content
+                        });
+                        break;
+                    case 'warning':
+                        this.$Modal.warning({
+                            title: title,
+                            content: content
+                        });
+                        break;
+                    case 'error':
+                        this.$Modal.error({
+                            title: title,
+                            content: content
+                        });
+                        break;
+                }
             }
         }
     }
