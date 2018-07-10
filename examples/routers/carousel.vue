7e77ba4e545ebae569c90bc79a81fd6cf8c2e86c
xujiabin <xujiabin0114@163.com>
2018-07-10 11:12:08 +0800

    ie9 轮播图

--- examples/routers/carousel.vue
+++ examples/routers/carousel.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Carousel v-model="v1" dots="outside" @on-change="hc" :autoplay="false">
+        <Carousel v-model="v1" dots="outside" @on-change="hc" :autoplay="true">
             <Carousel-item>
                 <div class="demo-case-img">
                     <img src="https://file.iviewui.com/dist/84b3cded4163febf9dae090e76129322.png">
@@ -8,12 +8,12 @@
             </Carousel-item>
             <Carousel-item>
                 <div class="demo-case-img">
-                    <img src="https://file.iviewui.com/dist/84b3cded4163febf9dae090e76129322.png">
+                    <img src="https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_ca79a146.png">
                 </div>
             </Carousel-item>
             <Carousel-item>
                 <div class="demo-case-img">
-                    <img src="https://file.iviewui.com/dist/84b3cded4163febf9dae090e76129322.png">
+                    <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2368407448,2365500091&fm=173&app=25&f=JPEG?w=550&h=315&s=BFA878234F5C56431CC4A5CE0100E0B2">
                 </div>
             </Carousel-item>
         </Carousel>
--- src/components/carousel/carousel.vue
+++ src/components/carousel/carousel.vue
@@ -30,7 +30,47 @@
     import { on, off } from '../../utils/dom';
 
     const prefixCls = 'ivu-carousel';
-
+    // function IEVersion() {
+    //     var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
+    //     var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器  
+    //     var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器  
+    //     var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    //     if(isIE) {
+    //         var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+    //         reIE.test(userAgent);
+    //         var fIEVersion = parseFloat(RegExp["$1"]);
+    //         if(fIEVersion == 7) {
+    //             return 7;
+    //         } else if(fIEVersion == 8) {
+    //             return 8;
+    //         } else if(fIEVersion == 9) {
+    //             return 9;
+    //         } else if(fIEVersion == 10) {
+    //             return 10;
+    //         } else {
+    //             return 6;//IE版本<=7
+    //         }   
+    //     } else if(isEdge) {
+    //         return 'edge';//edge
+    //     } else if(isIE11) {
+    //         return 11; //IE11  
+    //     }else{
+    //         return -1;//不是ie浏览器
+    //     }
+    // }
+    // if(IEVersion() === 9) { 
+    //     //alert("IE 9.0");
+    //     // window.requestAnimationFrame = (function(){
+    //     //     return  window.requestAnimationFrame       || 
+    //     //         window.webkitRequestAnimationFrame || 
+    //     //         window.mozRequestAnimationFrame    || 
+    //     //         window.oRequestAnimationFrame      || 
+    //     //         window.msRequestAnimationFrame     || 
+    //     //         function( callback ){
+    //     //             window.setTimeout(callback, 1000 / 2);
+    //     //         };
+    //     // })();
+    // } 
     export default {
         name: 'Carousel',
         components: { Icon },
