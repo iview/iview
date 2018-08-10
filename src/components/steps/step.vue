689657d6cc89a4fb1827a9ed8d038c331977e881
王奇\shuyong <599489502@qq.com>
2018-08-10 16:28:35 +0800

    j

--- src/components/steps/step.vue
+++ src/components/steps/step.vue
@@ -1,25 +1,45 @@
+<style>
+
+</style>
 <template>
     <div :class="wrapClasses" :style="styles">
         <div :class="[prefixCls + '-tail']"><i></i></div>
         <div :class="[prefixCls + '-head']">
             <div :class="[prefixCls + '-head-inner']">
-                <span v-if="!icon && status != 'finish' && status != 'error'">{{ stepNumber }}</span>
+                <span v-if="!icon && currentStatus != 'finish' && currentStatus != 'error'">{{ stepNumber }}</span>
                 <span v-else :class="iconClasses"></span>
             </div>
         </div>
         <div :class="[prefixCls + '-main']">
             <div :class="[prefixCls + '-title']">{{ title }}</div>
-            <div v-if="content" :class="[prefixCls + '-content']">{{ content }}</div>
+            <slot>
+                <!--stepType === true 使用新版-->
+                <div v-show="stepType==='true'">
+                    <!-- 事件 eventValue 事件显示的value eventTri自定义事件名 -->
+                    <div v-if="eventValue" v-on:click="eventTri" style="color: #63B8FF;cursor: pointer" :class="[prefixCls + '-content']">{{ eventValue }}</div>
+                    <!-- eventTri自定义事件名 isHide是否超出三个隐藏  content 内容 -->
+                    <Tooltip  v-if="content" max-width="200" placement="top" :transfer="true" style="display:block" :content='content'>
+                        <div v-on:click="eventTri" v-bind:style="isHide==='true'?'overflow: hidden;text-overflow:ellipsis;white-space: nowrap;width: 90px;':''" :class="[prefixCls + '-content']">{{ content }}</div>
+                    </Tooltip>
+                </div>
+                <!--不使用新版-->
+                <div v-show="stepType==='false'" v-if="content" :class="[prefixCls + '-content']">{{ content }}</div>
+                <!--footer 页脚-->
+                <div v-if="footer" :class="[prefixCls + '-content']">{{ footer }}</div>
+            </slot>
         </div>
     </div>
 </template>
 <script>
+    import Emitter from '../../mixins/emitter';
     import { oneOf } from '../../utils/assist';
 
     const prefixCls = 'ivu-steps';
     const iconPrefixCls = 'ivu-icon';
 
     export default {
+        name: 'Step',
+        mixins: [ Emitter ],
         props: {
             status: {
                 validator (value) {
@@ -33,8 +53,21 @@
             content: {
                 type: String
             },
+            footer: {
+                type: String
+            },
+            isHide:{
+                type: String,
+            },
             icon: {
                 type: String
+            },
+            eventValue: {
+                type: String
+            },
+            stepType: {
+                type: String,
+                default: 'false'
             }
         },
         data () {
@@ -42,14 +75,15 @@
                 prefixCls: prefixCls,
                 stepNumber: '',
                 nextError: false,
-                total: 1
+                total: 1,
+                currentStatus: ''
             };
         },
         computed: {
             wrapClasses () {
                 return [
                     `${prefixCls}-item`,
-                    `${prefixCls}-status-${this.status}`,
+                    `${prefixCls}-status-${this.currentStatus}`,
                     {
                         [`${prefixCls}-custom`]: !!this.icon,
                         [`${prefixCls}-next-error`]: this.nextError
@@ -62,9 +96,9 @@
                 if (this.icon) {
                     icon = this.icon;
                 } else {
-                    if (this.status == 'finish') {
+                    if (this.currentStatus == 'finish') {
                         icon = 'ios-checkmark-empty';
-                    } else if (this.status == 'error') {
+                    } else if (this.currentStatus == 'error') {
                         icon = 'ios-close-empty';
                     }
                 }
@@ -84,10 +118,26 @@
             }
         },
         watch: {
-            status () {
-                if (this.status == 'error') {
+            status (val) {
+                this.currentStatus = val;
+                if (this.currentStatus == 'error') {
                     this.$parent.setNextError();
                 }
+            }
+        },
+        created () {
+            this.currentStatus = this.status;
+        },
+        mounted () {
+            this.dispatch('Steps', 'append');
+
+        },
+        beforeDestroy () {
+            this.dispatch('Steps', 'remove');
+        },
+        methods: {
+            eventTri () {
+                this.$emit('triEvent');
             }
         }
     };
