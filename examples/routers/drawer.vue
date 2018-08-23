749665eee776ecddd45aa8a8b331b8f23f15b5dc
梁灏 <admin@aresn.com>
2018-08-23 15:40:26 +0800

    add props

---
+++ examples/routers/drawer.vue
@@ -0,0 +1,75 @@
+<template>
+    <Row :gutter="16">
+        <i-col span="12">
+            <Card title="horizontal divider">
+                <div>
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                    <Divider/>
+
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                    <Divider>iView </Divider>
+
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                    <Divider dashed/>
+
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                    <Divider orientation="left">iView</Divider>
+
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                    <Divider orientation="right">iView</Divider>
+
+                    <p>
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                        iView is a set of UI components and widgets built on Vue.js.
+                    </p>
+
+                </div>
+            </Card>
+        </i-col>
+        <i-col span="12">
+            <Card title="vertical divider">
+                <div>
+                    iView
+                    <Divider type="vertical" />
+                    <a href="#">Components</a>
+                    <Divider type="vertical" />
+                    <a href="#">Divider</a>
+                </div>
+            </Card>
+        </i-col>
+    </Row>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
--- src/components/drawer/drawer.vue
+++ src/components/drawer/drawer.vue
@@ -2,11 +2,73 @@
     <div></div>
 </template>
 <script>
+    import Icon from '../icon';
+    import { oneOf } from '../../utils/assist';
+    import TransferDom from '../../directives/transfer-dom';
+    import ScrollbarMixins from '../modal/mixins-scrollbar';
+
     const prefixCls = 'ivu-drawer';
 
     export default {
         name: 'Drawer',
+        mixins: [ ScrollbarMixins ],
+        components: { Icon },
+        directives: { TransferDom },
         props: {
+            value: {
+                type: Boolean,
+                default: false
+            },
+            title: {
+                type: String
+            },
+            width: {
+                type: [Number, String],
+                default: 256
+            },
+            closable: {
+                type: Boolean,
+                default: true
+            },
+            maskClosable: {
+                type: Boolean,
+                default: true
+            },
+            mask: {
+                type: Boolean,
+                default: true
+            },
+            maskStyle: {
+                type: Object
+            },
+            scrollable: {
+                type: Boolean,
+                default: false
+            },
+            placement: {
+                validator (value) {
+                    return oneOf(value, ['left', 'right']);
+                },
+                default: 'right'
+            },
+            zIndex: {
+                type: Number,
+                default: 1000
+            },
+            transfer: {
+                type: Boolean,
+                default () {
+                    return !this.$IVIEW || this.$IVIEW.transfer === '' ? true : this.$IVIEW.transfer;
+                }
+            },
+        },
+        data () {
+            return {
+                prefixCls: prefixCls,
+                visible: this.value,
+            };
+        },
+        methods: {
 
         }
     };
