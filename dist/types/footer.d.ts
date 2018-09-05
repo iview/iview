7a096c87632ea5d7485d17181eb338ce3db95ff2
jk2K <andy1945jp@gmail.com>
2018-09-05 16:51:06 +0800

    refactor: ignore dist folder

--- dist/types/footer.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for iview 3.1.0
-// Project: https://github.com/iview/iview
-// Definitions by: yangdan
-// Definitions: https://github.com/yangdan8/iview.git
-import Vue, { VNode } from 'vue';
-
-export declare interface Footer extends Vue {
-}
