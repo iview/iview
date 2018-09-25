bef01c91c1afb23d113ebd2e1c99f9b2264ccdd8
梁灏 <admin@aresn.com>
2018-09-25 09:45:24 +0800

    it will make a new bug, so revert

--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -359,7 +359,7 @@
                 /**
                  * Not sure why use hasDefaultSelected #4273
                  * */
-                // let hasDefaultSelected = slotOptions.some(option => this.query === option.key);
+                let hasDefaultSelected = slotOptions.some(option => this.query === option.key);
                 for (let option of slotOptions) {
 
                     const cOptions = option.componentOptions;
@@ -383,13 +383,10 @@
                         if (cOptions.children.length > 0) selectOptions.push({...option});
                     } else {
                         // ignore option if not passing filter
-                        //if (!hasDefaultSelected) {
-                        //    const optionPassesFilter = this.filterable ? this.validateOption(cOptions) : option;
-                        //    if (!optionPassesFilter) continue;
-                        //}
-
-                        const optionPassesFilter = this.filterable ? this.validateOption(cOptions) : option;
-                        if (!optionPassesFilter) continue;
+                        if (!hasDefaultSelected) {
+                            const optionPassesFilter = this.filterable ? this.validateOption(cOptions) : option;
+                            if (!optionPassesFilter) continue;
+                        }
 
                         optionCounter = optionCounter + 1;
                         selectOptions.push(this.processOption(option, selectedValues, optionCounter === currentIndex));
