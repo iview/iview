69a844010eb46b6193145e10eb578848d3d9a652
梁灏 <admin@aresn.com>
2018-10-23 11:21:31 +0800

    fix #4651

--- examples/routers/time.vue
+++ examples/routers/time.vue
@@ -5,6 +5,8 @@
         <Time :time="time2" />
         <br>
         <Time :time="time3" :interval="1" />
+        <br>
+        <Time :time="time4" />
     </div>
 </template>
 <script>
@@ -13,8 +15,9 @@
             return {
                 time1: (new Date()).getTime() - 60 * 59 * 1000,
                 time2: (new Date()).getTime() - 86400 * 3 * 1000,
-                time3: (new Date()).getTime() - 1 * 1000
-            }
+                time3: (new Date()).getTime() - 1 * 1000,
+                time4: (new Date()).getTime() - 86400 * 60 * 1000,
+            };
         }
-    }
+    };
 </script>
