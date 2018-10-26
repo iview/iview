3bb58e69f592958edbbebd7489a292147d2dce1d
Rookie_Zoe <zoe@rushb.cn>
2018-10-26 14:32:21 +0800

    [bug-fix] issue nubmer: #4718

--- examples/routers/color-picker.vue
+++ examples/routers/color-picker.vue
@@ -1,16 +1,18 @@
 <template>
     <div style="margin: 100px;">
-        {{ color1 }}
-        <ColorPicker v-model="color1" />
-        <ColorPicker v-model="color2" :editable="false"/>
+        <div>{{ color1 }}, {{ color2 }}, {{ color3 }}</div>
+        <ColorPicker v-model="color1" recommend />
+        <ColorPicker v-model="color2" recommend transfer/>
+        <ColorPicker v-model="color3" :editable="false"/>
     </div>
 </template>
 <script>
     export default {
         data () {
             return {
-                color1: '#19be6b',
-                color2: ''
+                color1: '#19BE6B',
+                color2: '#19BE6B',
+                color3: ''
             };
         }
     };
