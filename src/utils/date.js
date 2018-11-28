7f76a43c99e8476c851955c028b7fb2aa81fba5c
xiangweiping103 <xiangweiping103@pingan.com.cn>
2018-11-28 20:52:01 +0800

    fix: util/data.js should be writted in es module

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
 // 把 YYYY-MM-DD 改成了 yyyy-MM-dd
-(function (main) {
+
     'use strict';
 
     /**
@@ -307,13 +307,4 @@
     };
 
     /* istanbul ignore next */
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = fecha;
-    } else if (typeof define === 'function' && define.amd) {
-        define(function () {
-            return fecha;
-        });
-    } else {
-        main.fecha = fecha;
-    }
-})(this);
+export default fecha
