7b35c4ca2dc086f51711ae1ef3714a871a6d96b8
梁灏 <admin@aresn.com>
2018-12-18 14:17:09 +0800

    release 3.2.0

--- dist/iview.js
+++ dist/iview.js
@@ -27123,7 +27123,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.2.0-rc.2',
+    version: '3.2.0',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
