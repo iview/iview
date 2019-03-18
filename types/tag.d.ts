b63e3fc9bb468d60324d81c930d1ac0e6f2b98e8
yangd <yangd@pmac.com.cn>
2019-03-18 12:46:49 +0800

    修改d.ts版本号为3.3.1

--- types/tag.d.ts
+++ types/tag.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for iview 3.1.0
+// Type definitions for iview 3.3.1
 // Project: https://github.com/iview/iview
 // Definitions by: yangdan
 // Definitions: https://github.com/yangdan8/iview.git
