a6b98cde429327ce955c1e906c80b8555ccdff21
destiny-wenlun <1070586554@qq.com>
2019-06-03 17:19:26 +0800

    1、修改RadioGroup组件button类型的样式。 2、使RadioGroup组件支持Boolean类型

--- src/components/radio/radio-group.vue
+++ src/components/radio/radio-group.vue
@@ -4,94 +4,96 @@
     </div>
 </template>
 <script>
-    import { oneOf, findComponentsDownward } from '../../utils/assist';
-    import Emitter from '../../mixins/emitter';
+import { oneOf, findComponentsDownward } from "../../utils/assist";
+import Emitter from "../../mixins/emitter";
 
-    const prefixCls = 'ivu-radio-group';
+const prefixCls = "ivu-radio-group";
 
-    let seed = 0;
-    const now = Date.now();
-    const getUuid = () => `ivuRadioGroup_${now}_${seed++}`;
+let seed = 0;
+const now = Date.now();
+const getUuid = () => `ivuRadioGroup_${now}_${seed++}`;
 
-    export default {
-        name: 'RadioGroup',
-        mixins: [ Emitter ],
-        props: {
-            value: {
-                type: [String, Number],
-                default: ''
+export default {
+    name: "RadioGroup",
+    mixins: [Emitter],
+    props: {
+        value: {
+            type: [String, Number, Boolean],
+            default: ""
+        },
+        size: {
+            validator(value) {
+                return oneOf(value, ["small", "large", "default"]);
             },
-            size: {
-                validator (value) {
-                    return oneOf(value, ['small', 'large', 'default']);
-                },
-                default () {
-                    return !this.$IVIEW || this.$IVIEW.size === '' ? 'default' : this.$IVIEW.size;
-                }
-            },
-            type: {
-                validator (value) {
-                    return oneOf(value, ['button']);
-                }
-            },
-            vertical: {
-                type: Boolean,
-                default: false
-            },
-            name: {
-                type: String,
-                default: getUuid
+            default() {
+                return !this.$IVIEW || this.$IVIEW.size === ""
+                    ? "default"
+                    : this.$IVIEW.size;
             }
         },
-        data () {
-            return {
-                currentValue: this.value,
-                childrens: []
-            };
-        },
-        computed: {
-            classes () {
-                return [
-                    `${prefixCls}`,
-                    {
-                        [`${prefixCls}-${this.size}`]: !!this.size,
-                        [`ivu-radio-${this.size}`]: !!this.size,
-                        [`${prefixCls}-${this.type}`]: !!this.type,
-                        [`${prefixCls}-vertical`]: this.vertical
-                    }
-                ];
+        type: {
+            validator(value) {
+                return oneOf(value, ["button"]);
             }
         },
-        mounted () {
+        vertical: {
+            type: Boolean,
+            default: false
+        },
+        name: {
+            type: String,
+            default: getUuid
+        }
+    },
+    data() {
+        return {
+            currentValue: this.value,
+            childrens: []
+        };
+    },
+    computed: {
+        classes() {
+            return [
+                `${prefixCls}`,
+                {
+                    [`${prefixCls}-${this.size}`]: !!this.size,
+                    [`ivu-radio-${this.size}`]: !!this.size,
+                    [`${prefixCls}-${this.type}`]: !!this.type,
+                    [`${prefixCls}-vertical`]: this.vertical
+                }
+            ];
+        }
+    },
+    mounted() {
+        this.updateValue();
+    },
+    methods: {
+        updateValue() {
+            this.childrens = findComponentsDownward(this, "Radio");
+            if (this.childrens) {
+                this.childrens.forEach(child => {
+                    child.currentValue = this.currentValue === child.label;
+                    child.group = true;
+                });
+            }
+        },
+        change(data) {
+            this.currentValue = data.value;
             this.updateValue();
-        },
-        methods: {
-            updateValue () {
-                this.childrens = findComponentsDownward(this, 'Radio');
-                if (this.childrens) {
-                    this.childrens.forEach(child => {
-                        child.currentValue = this.currentValue === child.label;
-                        child.group = true;
-                    });
-                }
-            },
-            change (data) {
-                this.currentValue = data.value;
-                this.updateValue();
-                this.$emit('input', data.value);
-                this.$emit('on-change', data.value);
-                this.dispatch('FormItem', 'on-form-change', data.value);
-            }
-        },
-        watch: {
-            value () {
-                if(this.currentValue !== this.value){
-                    this.currentValue = this.value;
-                    this.$nextTick(()=>{
-                        this.updateValue();
-                    });
-                }
+            this.$emit("input", data.value);
+            this.$emit("on-change", data.value);
+            this.dispatch("FormItem", "on-form-change", data.value);
+        }
+    },
+    watch: {
+        value() {
+            if (this.currentValue !== this.value) {
+                this.currentValue = this.value;
+                this.$nextTick(() => {
+                    this.updateValue();
+                });
             }
         }
-    };
+    }
+};
 </script>
