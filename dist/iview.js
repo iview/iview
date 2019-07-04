afd80fa36fca5da4d9a7bb42e054361ede5f6ba5
robin <lb.robin1991@gmail.com>
2019-07-04 15:05:51 +0800

    publish: npm publish

--- dist/iview.js
+++ dist/iview.js
@@ -28534,7 +28534,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '3.4.2',
+    version: '1.0.0',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
