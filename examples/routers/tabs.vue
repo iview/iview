e61df0d0386899f5b42446104e4fd410dfd8c3e5
mo.duan <mo.duan@tendcloud.com>
2019-08-26 16:42:17 +0800

    6203 fixed
    #6203 bug fixed

--- examples/routers/tabs.vue
+++ examples/routers/tabs.vue
@@ -1,3 +1,60 @@
+<!--<template>-->
+<!--    <Tabs :animated="false" value="name1">-->
+<!--        <Tab-pane label="标签一" name="name1">-->
+<!--            <Table :columns="columns1"></Table>-->
+<!--        </Tab-pane>-->
+<!--        <Tab-pane label="标签二" name="name2">-->
+<!--            <Table :columns="columns1"></Table>-->
+<!--        </Tab-pane>-->
+<!--        <Tab-pane label="标签三" name="name3">-->
+<!--            <Table :columns="columns1"  ></Table>-->
+<!--        </Tab-pane>-->
+<!--    </Tabs>-->
+<!--</template>-->
+<!--<script>-->
+<!--    export default {-->
+<!--        data() {-->
+<!--            return {-->
+<!--                columns1: [-->
+<!--                    {-->
+<!--                        title: '姓名',-->
+<!--                        key: 'name'-->
+<!--                    },-->
+<!--                    {-->
+<!--                        title: '年龄',-->
+<!--                        key: 'age'-->
+<!--                    },-->
+<!--                    {-->
+<!--                        title: '地址',-->
+<!--                        key: 'address'-->
+<!--                    }-->
+<!--                ],-->
+<!--                data1: [-->
+<!--                    {-->
+<!--                        name: '王小明',-->
+<!--                        age: 18,-->
+<!--                        address: '北京市朝阳区芍药居'-->
+<!--                    },-->
+<!--                    {-->
+<!--                        name: '张小刚',-->
+<!--                        age: 25,-->
+<!--                        address: '北京市海淀区西二旗'-->
+<!--                    },-->
+<!--                    {-->
+<!--                        name: '李小红',-->
+<!--                        age: 30,-->
+<!--                        address: '上海市浦东新区世纪大道'-->
+<!--                    },-->
+<!--                    {-->
+<!--                        name: '周小伟',-->
+<!--                        age: 26,-->
+<!--                        address: '深圳市南山区深南大道'-->
+<!--                    }-->
+<!--                ]-->
+<!--            }-->
+<!--        }-->
+<!--    }-->
+<!--</script>-->
 <template>
     <div>
         <Button @click="showSecond = !showSecond">change</Button>
