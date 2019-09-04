25cba38b459de877b497c584d3a650fe89c693bd
梁灏 <admin@aresn.com>
2019-09-04 15:53:32 +0800

    Progress prop strokeColor support Array type, and support the linear-gradient color

--- examples/routers/progress.vue
+++ examples/routers/progress.vue
@@ -15,10 +15,20 @@
         <Divider></Divider>
 
         <Progress :percent="10" :stroke-width="20" text-inside></Progress>
+
+        <Divider></Divider>
+
+        <Progress :percent="90" :stroke-color="color1"></Progress>
+        <Progress :percent="90" :stroke-color="color2" status="active"></Progress>
     </div>
 </template>
 <script>
     export default {
-
+        data () {
+            return {
+                color1: '#ff6600',
+                color2: ['#108ee9', '#87d068']
+            }
+        }
     }
 </script>
