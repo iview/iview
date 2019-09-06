81395eca7d0f6ed3c52ace623e8235cca130c509
梁灏 <admin@aresn.com>
2019-09-06 10:54:50 +0800

    Tabs support scroll

--- examples/routers/tabs.vue
+++ examples/routers/tabs.vue
@@ -1,8 +1,10 @@
 <template>
-    <Tabs type="card">
-        <TabPane v-for="tab in tabs" :key="tab" :label="'标签' + tab">标签{{ tab }}</TabPane>
-        <Button @click="handleTabsAdd" size="small" slot="extra">增加</Button>
-    </Tabs>
+    <div style="height: 5000px">
+        <Tabs type="card">
+            <TabPane v-for="tab in tabs" :key="tab" :label="'标签' + tab">标签{{ tab }}</TabPane>
+            <Button @click="handleTabsAdd" size="small" slot="extra">增加</Button>
+        </Tabs>
+    </div>
 </template>
 <script>
     export default {
