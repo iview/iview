aaa7e9927c5c6751ec02c1205012a81fcd925623
梁灏 <admin@aresn.com>
2019-09-11 14:27:19 +0800

    Update select.vue

--- examples/routers/select.vue
+++ examples/routers/select.vue
@@ -1,52 +1,52 @@
 <template>
     <div style="margin: 100px;">
-<!--        <Select v-model="model1" filterable style="width:200px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model1" filterable style="width:200px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select v-model="model10" filterable :max-tag-count="2" multiple style="width:400px">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model10" filterable :max-tag-count="2" multiple style="width:400px">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select v-model="model10" filterable :max-tag-count="2" multiple style="width:400px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model10" filterable :max-tag-count="2" multiple style="width:400px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <br><br>-->
+        <br><br>
 
-<!--        <Select v-model="model1" style="width:200px">-->
-<!--            <Icon type="ios-alarm" slot="prefix" color="red" />-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model1" style="width:200px">
+            <Icon type="ios-alarm" slot="prefix" color="red" />
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select v-model="model1" style="width:200px">-->
-<!--            <Avatar src="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar" slot="prefix" size="small" />-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model1" style="width:200px">
+            <Avatar src="https://dev-file.iviewui.com/userinfoPDvn9gKWYihR24SpgC319vXY8qniCqj4/avatar" slot="prefix" size="small" />
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select v-model="model10" :max-tag-count="3" :max-tag-placeholder="more" multiple style="width:400px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select v-model="model10" :max-tag-count="3" :max-tag-placeholder="more" multiple style="width:400px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <br><br>-->
+        <br><br>
 
-<!--        <Select size="small" v-model="model1" style="width:200px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select size="small" v-model="model1" style="width:200px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select size="small" v-model="model10" multiple style="width:400px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select size="small" v-model="model10" multiple style="width:400px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <br><br>-->
+        <br><br>
 
-<!--        <Select size="large" v-model="model1" style="width:200px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select size="large" v-model="model1" style="width:200px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
 
-<!--        <Select size="large" v-model="model10" multiple style="width:400px" prefix="ios-albums">-->
-<!--            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>-->
-<!--        </Select>-->
+        <Select size="large" v-model="model10" multiple style="width:400px" prefix="ios-albums">
+            <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
         <Divider></Divider>
         <Select v-model="model13" filterable allow-create @on-create="handleCreate" style="width:260px">
             <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
