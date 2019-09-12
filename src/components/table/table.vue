8607307e1761d58d4a3b65a1960a44d3014819b8
mo.duan <mo.duan@tendcloud.com>
2019-09-12 13:48:44 +0800

    #6255 #6258 Bug fixed

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -967,7 +967,9 @@
 
             this.$on('on-visible-change', (val) => {
                 if (val) {
-                    this.handleResize();
+                    this.$nextTick(()=>{
+                        this.handleResize();
+                    });
                 }
             });
         },
