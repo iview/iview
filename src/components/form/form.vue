e522134d248832eeb460dcfeaccfec1e8c33121b
梁灏 <admin@aresn.com>
2019-09-16 18:46:14 +0800

    change Form provide name

--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -49,8 +49,8 @@
                 default: false
             }
         },
-        provide() {
-            return { form : this };
+        provide () {
+            return { FormInstance : this };
         },
         data () {
             return {
