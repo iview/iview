49f71082703a22996bc77ac444ad4bfc2453ab2e
梁灏 <admin@aresn.com>
2019-09-23 11:35:54 +0800

    update Icon style

--- examples/routers/button.vue
+++ examples/routers/button.vue
@@ -277,10 +277,43 @@
         <Button icon="md-alert">
             左右图标
         </Button>
+        <Divider></Divider>
+        <div>
+            <RadioGroup v-model="buttonSize" type="button">
+                <Radio label="large">Large</Radio>
+                <Radio label="default">Default</Radio>
+                <Radio label="small">small</Radio>
+            </RadioGroup>
+            <br><br>
+            <Button :size="buttonSize" type="primary">Primary</Button>
+            <Button :size="buttonSize" type="default">Default</Button>
+            <Button :size="buttonSize" type="dashed">Dashed</Button>
+            <Button :size="buttonSize" type="text">Text</Button>
+            <br><br>
+            <Button :size="buttonSize" icon="md-download" type="primary" shape="circle"></Button>
+            <Button :size="buttonSize" icon="md-download" type="primary">Download</Button>
+            <br><br>
+            <ButtonGroup :size="buttonSize">
+                <Button :size="buttonSize" type="primary">
+                    <Icon type="ios-arrow-back" />
+                    Backward
+                </Button>
+                <Button :size="buttonSize" type="primary">
+                    Forward
+                    <Icon type="ios-arrow-forward" />
+                </Button>
+            </ButtonGroup>
+        </div>
+        <Divider></Divider>
     </div>
 </template>
 <script>
     export default {
+        data () {
+            return {
+                buttonSize: 'large'
+            }
+        },
         methods: {
             hc (data) {
                 console.log(data);
