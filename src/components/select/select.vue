a6068736cb85509a591287cc47ea64b1765cd9a3
mo.duan <mo.duan@tendcloud.com>
2019-10-08 11:46:18 +0800

    bug fixed #6301

--- src/components/select/select.vue
+++ src/components/select/select.vue
@@ -710,7 +710,7 @@
                 if (value === '') this.values = [];
                 else if (checkValuesNotEqual(value,publicValue,values)) {
                     this.$nextTick(() => this.values = getInitialValue().map(getOptionData).filter(Boolean));
-                    this.dispatch('FormItem', 'on-form-change', this.publicValue);
+                    !(Array.isArray(value)) && this.dispatch('FormItem', 'on-form-change', this.publicValue);
                 }
             },
             values(now, before){
