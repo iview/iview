8b7c5c249a6907c516254aea6add0e095b49fa2c
lifenglu <fengluhome@163.com>
2019-10-14 11:42:58 +0800

    bug fixed #4441

--- src/components/auto-complete/auto-complete.vue
+++ src/components/auto-complete/auto-complete.vue
@@ -13,7 +13,7 @@
         remote
         auto-complete
         :remote-method="remoteMethod"
-        @on-change="handleChange"
+        @on-select="handleSelect"
         @on-clickoutside="handleClickOutside"
         :transfer="transfer">
         <slot name="input">
@@ -152,9 +152,10 @@
             remoteMethod (query) {
                 this.$emit('on-search', query);
             },
-            handleChange (val) {
+            handleSelect (val) {
                 if (val === undefined || val === null) return;
                 this.currentValue = val;
+
                 this.$refs.input.blur();
                 this.$emit('on-select', val);
             },
