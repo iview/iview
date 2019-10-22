410a5fbca7ddbd9e71cae89d1150ec012c5d7460
梁灏 <admin@aresn.com>
2019-10-22 16:12:05 +0800

    release 4.0.2

--- dist/iview.js
+++ dist/iview.js
@@ -29522,7 +29522,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '4.0.2-rc.1',
+    version: '4.0.2',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
