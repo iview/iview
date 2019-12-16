3baf0595c2b3337aa7671d306c136afadbf8babf
梁灏 <admin@aresn.com>
2019-12-16 10:43:44 +0800

    fix AutoComplete transfer prop error in global setting

--- src/components/auto-complete/auto-complete.vue
+++ src/components/auto-complete/auto-complete.vue
@@ -96,7 +96,7 @@
             transfer: {
                 type: Boolean,
                 default () {
-                    return this.$IVIEW.transfer === '' ? false : this.$IVIEW.transfer;
+                    return !this.$IVIEW || this.$IVIEW.transfer === '' ? false : this.$IVIEW.transfer;
                 }
             },
             name: {
