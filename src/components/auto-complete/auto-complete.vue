dd35ff0f8c74007a64c1957001ce718163c97551
梁灏 <admin@aresn.com>
2019-12-16 11:30:00 +0800

    fix AutoComplete on-select event error

--- src/components/auto-complete/auto-complete.vue
+++ src/components/auto-complete/auto-complete.vue
@@ -13,7 +13,7 @@
         remote
         auto-complete
         :remote-method="remoteMethod"
-        @on-change="handleChange"
+        @on-select="handleSelect"
         @on-clickoutside="handleClickOutside"
         :transfer="transfer">
         <slot name="input">
@@ -152,7 +152,8 @@
             remoteMethod (query) {
                 this.$emit('on-search', query);
             },
-            handleChange (val) {
+            handleSelect (option) {
+                const val = option.value;
                 if (val === undefined || val === null) return;
                 this.currentValue = val;
                 this.$refs.input.blur();
