a2828f909799dc461498f73df86b8d0319402bdb
qii404 <shiboqi123@gmail.com>
2020-08-12 18:00:26 +0800

    进度条多次点击同一位置，不会触发on-change事件
    复现情景： https://jsfiddle.net/qii404/7nzu6p8j/4/

--- src/components/slider/slider.vue
+++ src/components/slider/slider.vue
@@ -341,10 +341,10 @@
                 this.currentValue = [...value];
 
                 if (!this.dragging) {
-                    if (this.currentValue[index] !== this.oldValue[index]) {
-                        this.emitChange();
-                        this.oldValue[index] = this.currentValue[index];
-                    }
+                    // if (this.currentValue[index] !== this.oldValue[index]) {
+                    this.emitChange();
+                    //     this.oldValue[index] = this.currentValue[index];
+                    // }
                 }
             },
             handleDecimal(pos,step){
