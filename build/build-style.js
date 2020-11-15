df272dbc5aa53a98a872be510e762f4456ec4811
jinyongcheng <jinyongcheng@cmhi.chinamobile.com>
2020-11-15 16:38:25 +0800

    更新打包配置

--- build/build-style.js
+++ build/build-style.js
@@ -1,25 +1,48 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const less = require('gulp-less');
-const rename = require('gulp-rename');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
+const less = require("gulp-less");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
 
 // 编译less
-gulp.task('css', function () {
-    gulp.src('../src/styles/index.less')
+
+function css(cb) {
+    gulp.src("../src/styles/index.less")
         .pipe(less())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie > 8']
-        }))
+        .pipe(
+            autoprefixer()
+        )
         .pipe(cleanCSS())
-        .pipe(rename('iview.css'))
-        .pipe(gulp.dest('../dist/styles'));
-});
+        .pipe(rename("iview.css"))
+        .pipe(gulp.dest("../dist/styles"));
+
+    cb();
+}
+
+/* gulp.task("css", function() {
+    gulp.src("../src/styles/index.less")
+        .pipe(less())
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions", "ie > 8"]
+            })
+        )
+        .pipe(cleanCSS())
+        .pipe(rename("iview.css"))
+        .pipe(gulp.dest("../dist/styles"));
+}); */
 
 // 拷贝字体文件
-gulp.task('fonts', function () {
-    gulp.src('../src/styles/common/iconfont/fonts/*.*')
-        .pipe(gulp.dest('../dist/styles/fonts'));
-});
+function fonts(cb) {
+    gulp.src("../src/styles/common/iconfont/fonts/*.*").pipe(
+        gulp.dest("../dist/styles/fonts")
+    );
+    cb();
+}
+/* gulp.task("fonts", function() {
+    gulp.src("../src/styles/common/iconfont/fonts/*.*").pipe(
+        gulp.dest("../dist/styles/fonts")
+    );
+}); */
 
-gulp.task('default', ['css', 'fonts']);
+gulp.task("default", gulp.series(css, fonts));
