7724380dc6ebd82277ef0699bc61a0f9af3d614e
yinchun.liu <1101032620@qq.com>
2021-11-03 14:50:53 +0800

    fix: 修复picker组件click-outside后仍然保留焦点

--- src/components/date-picker/picker.vue
+++ src/components/date-picker/picker.vue
@@ -417,6 +417,7 @@
                 if (e && e.type === 'focus') return; // just focus, don't open yet
                 if(!this.itemDisabled){
                     this.visible = true;
+                    e.target.focus();
                 }
             },
             handleBlur (e) {
@@ -425,6 +426,7 @@
                     return;
                 }
                 if (this.visible) {
+                    this.isFocused = false;
                     e.preventDefault();
                     return;
                 }
