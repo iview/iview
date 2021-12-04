df024beba639d05dc6714b5c1836ca05fc20a6f5
junyo <1KnowMyPP>
2021-12-04 15:26:25 +0800

    chore: 更新版本号到4.4.34

--- dist/iview.js
+++ dist/iview.js
@@ -31092,7 +31092,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '4.4.33',
+    version: '4.4.34',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
